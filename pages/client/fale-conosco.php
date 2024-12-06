12dd6ba8aa8542121b30d8443f976718108070d8
gcamargogy <camargogdy@hotmail.com>
2024-12-06 14:01:27 -0400

    task021 - Tela Contate-nos linkado

---
+++ pages/client/fale-conosco.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato ao Suporte</title>
+    <link rel="stylesheet" href="../../css/faleconosco.css">
+</head>
+<body>
+    <?php include "../../assets/home/menu-home-client.html"; ?>
+    <main>
+
+
+        <div class="caixa-guilherme">
+            <div class="box-guilherme">
+                <h1>Fale Conosco!</h1>
+                <h2 class="txt1-guilherme">Tem dúvidas, ou sugestões? Preencha o formuláro<br>ao lado e envie sua mensagem para
+                    nossa equipe.</h2></h2>
+                <h2 class="txt2-guilherme"><span class="negrito-guilherme">TELEFONE:</span> (67) 4002-9822.<br><span class="negrito-guilherme">E-MAIL:</span> contato@feirabosque.com.br.<br><span class="negrito-guilherme">ENDEREÇO:</span> Praça Bosque
+                    da Paz, Rua Kame Takaiassu, Carandá Bosque, MS.</h2>
+            </div>
+        </div>
+
+        
+        <div class="circulo-guilherme">
+            <img src="../../imgs/img-area-contate/boneco-contate.png" alt="Bosque da Paz">
+        </div>
+        <div class="circulo2-guilherme">
+            <img src="../../imgs/img-area-contate/seta-voltar.png" alt="Bosque da Paz">
+        </div>
+        <div class="imagem-guilherme">
+            <img src="../../imgs/imagens-bolas/azul-sem-fundo1.png" alt="Bosque da Paz">
+        </div>
+        <div class="imagem2-guilherme">
+            <img src="../../imgs/imagens-bolas/azul-sem-fundo2.png" alt="Bosque da Paz">
+        </div>
+        <div class="imagem3-guilherme">
+            <img src="../../imgs/imagens-bolas/azul-sem-fundo3.png" alt="Bosque da Paz">
+        </div>
+
+
+        <div class="form-box-guilherme">
+            <form action="#">
+                <div class="formulario-do-cadastro-guilherme">
+                    <label>Nome Completo</label>
+                    <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo." required>
+                </div>
+                <div class="formulario-do-cadastro-guilherme">
+                    <label>E-email</label>
+                    <input type="email" name="email" id="email" placeholder="Digite seu e-mail." required>
+                </div>
+                <div class="formulario-do-cadastro-guilherme">
+                    <label>Mensagem</label>
+                    <input type="text" name="mensagem" id="mensagem" placeholder="Digite sua mensagem." required>
+                </div>
+            </form>
+        </div>
+
+
+        <button class="enviar-guilherme">ENVIAR</button>
+
+    <script src="../js/main.js"></script>
+    </main>
+</body>
+</html>
