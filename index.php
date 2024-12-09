b039591637b894f600d3ab662029d366d0acd7f5
gcamargogy <camargogdy@hotmail.com>
2024-12-09 16:48:37 -0400

    rastramentov6

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php
-// Lista de IPs permitidos (somente IPv6)
-$allowed_ips = [
-    'fe80::756d:2022:8c78:1574%2' // IP permitido (substitua pelo IP correto)
+$ips_permitidos = [
+    // '::1'
 ];
 
-// Obter o IP do visitante
-$user_ip = $_SERVER['REMOTE_ADDR'];
+$ip_usuario = $_SERVER['REMOTE_ADDR'];
 
-// Verifica se o IP é válido e se é IPv6
-if (filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-    // Verificar se o IP está na lista de permitidos
-    if (in_array($user_ip, $allowed_ips)) {
-        // Se o IP for permitido, redireciona para a pasta desejada
-        header('Location: Todos/'); // Caminho para a pasta onde estão suas telas
-        exit; // Importante para garantir que o redirecionamento aconteça imediatamente
-    } else {
-        // Se o IP não for permitido, exibe uma mensagem e impede o acesso
-        header('HTTP/1.1 403 Forbidden');
-        echo "<h1>Acesso Negado</h1>";
-        echo "<p>Seu endereço IP ($user_ip) não está autorizado a acessar esta pasta.</p>";
+if(filter_var($ip_usuario, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+    if (in_array($ip_usuario, $ips_permitidos)) {
+        header("Location: Todos/");
         exit;
     }
-} else {
-    // Se não for IPv6, exibe uma mensagem de erro
-    header('HTTP/1.1 403 Forbidden');
-    echo "<h1>Acesso Negado</h1>";
-    echo "<p>Somente endereços IPv6 são permitidos.</p>";
+    else {
+        header("HTTP/1.1 403 Forbidden");
+        echo "<p>O seu endereço IP não está na lista de conexão permitida.</p>";
+        echo "<p>IP: ($ip_usuario)</p>";
+        exit;
+    }
+}
+else {
+    header("HTTP/1.1 403 Forbidden");
+    echo "<p>Esse tipo de acesso não está permitido.</p>";
+    echo "<p>Endereço IPV6 requerido.</p>";
     exit;
 }
 ?>
