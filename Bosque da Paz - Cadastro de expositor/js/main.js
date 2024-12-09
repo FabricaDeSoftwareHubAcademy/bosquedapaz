2177bb26ef34fd93e633e2eea05616d5561dc99d
Maykelly Castro <cezariomaykelly@gmail.com>
2024-12-09 14:30:19 -0400

    alteracoes

---
+++ Bosque da Paz - Cadastro de expositor/js/main.js
@@ -0,0 +1,19 @@
+document.getElementById("salvar-btn").addEventListener("click", function () {
+
+    const confirmacao = confirm("Você tem certeza que deseja salvar?");
+
+    if (confirmacao) {
+
+        // Ação ao confirmar
+
+        alert("Salvamento confirmado!");
+
+    } else {
+
+        // Ação ao cancelar
+
+        alert("Salvamento cancelado.");
+
+    }
+
+});
