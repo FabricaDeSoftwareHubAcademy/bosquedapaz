eaf216a91359e53ab15fdab8ccdff5c5daf430ff
gcamargogy <camargogdy@hotmail.com>
2024-12-09 14:54:57 -0400

    rastreamentov4

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-// Lista de IPs permitidos (somente IPv4)
+// Lista de IPs permitidos (somente IPv6)
 $allowed_ips = [
-    '127.0.0.1', // IP do localhost (IPv4)
-    '10.28.1.147' // Exemplo de IP permitido (substitua pelo IP correto)
+    '::1',           // IP do localhost (IPv6)
+    'fe80::756d:2022:8c78:1574%2'
 ];
 
 // Obter o IP do visitante
 $user_ip = $_SERVER['REMOTE_ADDR'];
 
-// Verifica se o IP é válido e se é IPv4
-if (filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+// Verifica se o IP é válido e se é IPv6
+if (filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
     // Verificar se o IP está na lista de permitidos
     if (!in_array($user_ip, $allowed_ips)) {
         // Se o IP não for permitido, exibe uma mensagem e impede o acesso
@@ -19,10 +19,10 @@ if (filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
         exit;
     }
 } else {
-    // Se não for IPv4, exibe uma mensagem de erro (se necessário)
+    // Se não for IPv6, exibe uma mensagem de erro
     header('HTTP/1.1 403 Forbidden');
     echo "<h1>Acesso Negado</h1>";
-    echo "<p>Somente endereços IPv4 são permitidos.</p>";
+    echo "<p>Somente endereços IPv6 são permitidos.</p>";
     exit;
 }
 ?>
