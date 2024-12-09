0d11a36e0c9e20ce1deab882d0f39c50bd9951c7
gcamargogy <camargogdy@hotmail.com>
2024-12-09 15:07:19 -0400

    rastreamentov5

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 // Lista de IPs permitidos (somente IPv6)
 $allowed_ips = [
-    '::1',           // IP do localhost (IPv6)
-    'fe80::756d:2022:8c78:1574%2'
+    'fe80::756d:2022:8c78:1574%2' // IP permitido (substitua pelo IP correto)
 ];
 
 // Obter o IP do visitante
@@ -11,7 +10,11 @@ $user_ip = $_SERVER['REMOTE_ADDR'];
 // Verifica se o IP é válido e se é IPv6
 if (filter_var($user_ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
     // Verificar se o IP está na lista de permitidos
-    if (!in_array($user_ip, $allowed_ips)) {
+    if (in_array($user_ip, $allowed_ips)) {
+        // Se o IP for permitido, redireciona para a pasta desejada
+        header('Location: Todos/'); // Caminho para a pasta onde estão suas telas
+        exit; // Importante para garantir que o redirecionamento aconteça imediatamente
+    } else {
         // Se o IP não for permitido, exibe uma mensagem e impede o acesso
         header('HTTP/1.1 403 Forbidden');
         echo "<h1>Acesso Negado</h1>";
