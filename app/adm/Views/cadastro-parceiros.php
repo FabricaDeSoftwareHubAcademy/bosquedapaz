d9dc96979f8533488f37f321bf91b7664a5e536c
Teodororibas <teodororibas@icloud.com>
2025-01-27 16:09:44 -0400

    parceiros

---
+++ app/adm/Views/cadastro-parceiros.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modelo de tela</title>
+    <link rel="stylesheet" href="../CSS/style-modelo-carrossel.css"> 
+    <link rel="stylesheet" href="../CSS/TelaParceiros.css">
+</head>
+<body>
+
+
+    <main class="principal">
+        <div class="box">
+            <h1 class="tela-titulo">CADASTRAR PARCEIROS</h1>
+            <div class="geral">
+                <div class="ec">
+                    <div class="lado-esquerdo">
+                        <h3 class="palavra-nome">Nome:</h3>
+                        <input class="input-nome" type="text">
+                        <h3 class="descrição-palavra">Descrição:<span class="opcional-palavra">(opcional)</span></h3>
+                        <input class="descrição" type="text">                   
+                    </div>
+                    
+                    <div class="meio-centro">                  
+                        <h3 class="logo-palavra">Logo:</h3>
+                        <img src="../img/editar-imagem.png" alt="Editar Imagem" class="editar-imagem">                 
+                    </div>
+                </div>
+                    
+                <div class="imagens-esquerda">                  
+                    <img src="../img/Vector.png" alt="Linha Central" class="linha-central">                   
+                    <img src="../img/imagem_2024-08-07_144830377-removebg-preview (1) 2.png" alt="Imagem Parceiros" class="imagem-parceiros">
+                </div>
+            </div>
+        
+            <div class="botoes">
+                <button class="btn-default-cadastrar">
+                    CADASTRAR
+                </button>                  
+            </div>
+        </div>
+       
+        
+    </main>
+
+
+    <div class="bolas-fundo">
+        <img src="../img/bola-verde1.png" alt="Bola Fundo 1" class="bola-verde1">
+        <img src="../img/bola-verde2.png" alt="Bola Fundo 2" class="bola-verde2">
+        <img src="../img/bola-rosa.png" alt="Bola Fundo 3" class="bola-rosa">
+    </div>
+
+    <script src="../js/main.js"></script>
+
+</body>
+</html>
