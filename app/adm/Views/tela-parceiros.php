f209dd1085866ec5e73a5c24bded409c41303274
Teodororibas <teodororibas@icloud.com>
2025-01-27 17:11:07 -0400

    parceiros

--- app/adm/Views/tela-parceiros.php
+++ app/adm/Views/tela-parceiros.php
@@ -1,53 +1,50 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Página para gerenciar parceiros e suas informações.">
-    <title>Modelo de Tela</title>
-    <link rel="stylesheet" href="../../../Public/css/css-adm/tela-parceiros.css">
-</head>
-
-<body>
 <?php include "../../../Public/assets/adm/menu-adm.html"?>
 
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Página para gerenciar parceiros e suas informações.">
+        <title>Parceiros</title>
+        <link rel="stylesheet" href="../../../Public/css/css-adm/tela-parceiros.css">
+        <link rel="stylesheet" href="../../../Public/css/menu-adm.css">
+    </head>
 
-    <main class="principal">
-        <div class="box">
-            <h1 class="tela-titulo">ÁREA PARCEIROS</h1>
+    <body>
+        <main class="principal">
+            <div class="box">
+                <h1 class="tela-titulo">ÁREA PARCEIROS</h1>
 
-            <div class="todo-conteudos">
-                <div class="div-parceiros parceiros-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 1" class="img-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 2" class="img-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 3" class="img-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 4" class="img-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 5" class="img-pref">
-                    <img src="../img/pref-cg.png" alt="Imagem de parceiro 6" class="img-pref">
-                </div>
-                <!-- <div class="div-parceiros parceiros-pref-dois">
-                </div> -->
-                
-                
-                <div class="geral">
-                    <img src="../img/Vector.png" alt="linha-central" class="linha-central">
-                    <div class="button-cadastrar">
-                        <button class="button">CADASTRAR</button>
+                <div class="todo-conteudos">
+                    <div class="div-parceiros parceiros-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 1" class="img-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 2" class="img-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 3" class="img-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 4" class="img-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 5" class="img-pref">
+                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 6" class="img-pref">
                     </div>
-                    <img src="../img/imagem_2024-08-07_144830377-removebg-preview (1) 2.png" alt="imagem-parceiros" class="img-g">
+                                     
+                    <div class="geral">
+                        <img src="../../../Public/imgs/img-parceiros.css/linha-central.png" alt="linha-central" class="linha-central">
+                        <div class="button-cadastrar">
+                            <button class="button">CADASTRAR</button>
+                        </div>
+                        <img src="../../../Public/imgs/img-parceiros.css/foto-parceiros.png" alt="imagem-parceiros" class="img-g">
+                    </div>
+                    
                 </div>
-                
+                    
             </div>
-                
+        </main>
+
+        <div class="bolas-fundo">
+            <img src="../../../Public/imgs/imagens-bolas/bola-verde1.png" alt="" class="bola-verde1">
+            <img src="../../../Public/imgs/imagens-bolas/bola-verde2.png" alt="" class="bola-verde2">
+            <img src="../../../Public/imgs/imagens-bolas/Elemento3.ElipseRosa.png" alt="" class="bola-rosa">
         </div>
-    </main>
 
-    <div class="bolas-fundo">
-        <img src="../../../Public/imgs/imagens-bolas/bola-verde1.png" alt="" class="bola-verde1">
-        <img src="../../../Public/imgs/imagens-bolas/bola-verde2.png" alt="" class="bola-verde2">
-        <img src="../../../Public/imgs/imagens-bolas/Elemento3.ElipseRosa.png" alt="" class="bola-rosa">
-    </div>
-
-    <script src="../js/main.js"></script>
-</body>
+        <script src="../js/main.js"></script>
+    </body>
 </html>
