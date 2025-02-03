68c5940d443a907c54a891291f8cc16c4fc3b030
gcamargogy <camargogdy@hotmail.com>
2025-02-03 14:43:32 -0400

    indexcontatemvc

--- app/client/Views/fale-conosco.php
+++ app/client/Views/fale-conosco.php
@@ -1,66 +1,61 @@
-<?php include "../../../Public/assets/home/menu-home.html"; ?>
+<?php include "../../../Public/assets/home/menu-home.html"?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contato ao Suporte</title>
+    
+    <title>Área de Suporte</title>
     <link rel="stylesheet" href="../../../Public/css/faleconosco.css">
-    <link rel="stylesheet" href="../../../Public/css/css-home/menu.css">
+
 </head>
 <body>
-    <?php include "../../assets/home/menu-home-client.html"; ?>
-    <main>
+    <div class="box-guilherme">
+        <section class="secao-formulario-guilherme">
+            <h1 class="titulo-formulario-guilherme">Fale Conosco!</h1>
 
+            <div class="area-formulario-guilherme">
+                <form class="formulario-guilherme">
+                    <label>Nome completo</label>
+                    <input type="text" name="campo1" id="campo-1" placeholder="Digite seu nome completo" required>
 
-        <div class="caixa-guilherme">
-            <div class="box-guilherme">
-                <h1>Fale Conosco!</h1>
-                <h2 class="txt1-guilherme">Tem dúvidas, ou sugestões? Preencha o formuláro<br>ao lado e envie sua mensagem para
-                    nossa equipe.</h2></h2>
-                <h2 class="txt2-guilherme"><span class="negrito-guilherme">TELEFONE:</span> (67) 4002-9822.<br><span class="negrito-guilherme">E-MAIL:</span> contato@feirabosque.com.br.<br><span class="negrito-guilherme">ENDEREÇO:</span> Praça Bosque
-                    da Paz, Rua Kame Takaiassu, Carandá Bosque, MS.</h2>
-            </div>
-        </div>
+                    <label>E-mail</label>
+                    <input type="email" name="campo2" id="campo-2" placeholder="exemplo@gmail.com" required>
 
-        
-        <div class="circulo-guilherme">
-            <img src="../../../Public/imgs/img-area-contate/boneco-contate.png" alt="Bosque da Paz">
-        </div>
-        <div class="circulo2-guilherme">
-            <img src="../../../Public/imgs/img-area-contate/seta-voltar.png" alt="Bosque da Paz">
-        </div>
-        <div class="imagem-guilherme">
-            <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo1.png" alt="Bosque da Paz">
-        </div>
-        <div class="imagem2-guilherme">
-            <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo2.png" alt="Bosque da Paz">
-        </div>
-        <div class="imagem3-guilherme">
-            <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo3.png" alt="Bosque da Paz">
-        </div>
-
-
-
-        <div class="form-box-guilherme">
-            <form action="#">
-                <div class="formulario-do-cadastro-guilherme">
-                    <label>Nome Completo</label>
-                    <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo." required>
-                </div>
-                <div class="formulario-do-cadastro-guilherme">
-                    <label>E-email</label>
-                    <input type="email" name="email" id="email" placeholder="Digite seu e-mail." required>
-                </div>
-                <div class="formulario-do-cadastro-guilherme">
                     <label>Mensagem</label>
-                    <input type="text" name="mensagem" id="mensagem" placeholder="Digite sua mensagem." required>
+                    <textarea name="campo3" class="campo-3-guilherme" placeholder="Digite aqui sua mensagem." required></textarea>
+                    
+                    <button class="botao-enviar-guilherme">Enviar</button>
+                </form>
+            </div>
+        </section>
+
+        <section class="secao-informacoes-guilherme">
+            <div class="informacao-introducao-guilherme">
+                <div class="fundo-guilherme">
+                    <img src="../../../Public/imgs/img-area-contate/boneco-contate.png" alt="boneco" class="imagem-boneco-guilherme">
                 </div>
-            </form>
-        </div>
+                <h1 class="msg-guilherme">Possui dúvidas, reclamações, sugestões ou problemas? Entre em contato com nossa equipe preenchendo o formulário</h1>
+            </div>
 
+            <div class="informacao-contatos-guilherme">
+                <div class="informacao-titulo-guilherme">
+                    <h1>OUTROS MEIOS DE CONTATO</h1>
+                </div>
+                <div class="contatos-guilherme">
+                    <h1 class="contato-guilherme telefone"><strong>TELEFONE: </strong>(67) 4002-9822.</h1>
+                    <h1 class="contato-guilherme email"><strong>E-MAIL: </strong><a href="https://www.letras.mus.br/palmeiras/397875/">contato@feirabosque.com.br</a></h1>
+                    <h1 class="contato-guilherme localizacao"><strong>LOCALIZAÇÃO: </strong> Rua Kame Takaiassu, Carandá Bosque, MS.</h1>
+                </div>
+            </div>
+        </section>
+    </div>
 
-        <button class="enviar-guilherme">ENVIAR</button>
-    </main>
+    <div class="decoracoes-guilherme">
+        <img src="../../../Public/imgs/img-area-contate/seta-voltar.png" alt="btnVoltar" class="botao-voltar-guilherme">
+        <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo1.png" alt="deco1" class="decoracao1-guilherme">
+        <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo2.png" alt="deco2" class="decoracao2-guilherme">
+        <img src="../../../Public/imgs/imagens-bolas/azul-sem-fundo3.png" alt="deco3" class="decoracao3-guilherme">
+    </div>
 </body>
 </html>
