1cf8bc1ba940cc106a132d39a87fd3c12156faac
Yan <yantmartins@live.com>
2025-02-04 15:23:21 -0400

    update links

---
+++ app/adm/Views/manage-events.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área Administrativa</title>
+    <script src="../../../Public/js/js-menu/js-menu.js" defer></script> 
+    <link rel="stylesheet" href="../../../Public/css/css-adm/style-manage-events.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+</head>
+<body>
+
+        <!-- Menu -->
+
+        <?php include "../../../Public/assets/adm/menu-adm.html"?>
+
+    <script src="../js/main.js"></script>
+    
+        <!-- Área Dos Botões -->
+
+    <main class="principal-manage-events">
+        
+        <div class="box-manage-events">
+        <div class="nome-area-manage-events">
+                <h1>Eventos</h1>
+            </div>
+
+                <div class="seta-voltar-manage-events">
+                <a href="Area-Adm.php">
+                <img src="../../../Public/imgs/img-area-adm/Captura de tela 2024-09-25 163826.png" alt="">
+                </a>
+                </div>
+
+                <div class="Botoes-manage-events">
+                    <a href="cadastro-evento.php" class="link-area-icon">
+                    <div class="area-icon-manage-events">
+                        <img src="../../../Public/imgs/img-area-adm/Vector Eventos.png" alt="">
+                        <p>Cadastrar evento</p>
+                    </div>
+                </div>
+                </a>
+                <div class="Botoes-manage-events">
+                    <a href="gerenciar-eventos.php" class="link-area-icon">
+                    <div class="area-icon-manage-events">
+                        <img src="../../../Public/imgs/img-area-adm/Vector Eventos.png" alt="">
+                        <p>Gerenciar evento </p>
+                    </div>
+                </div>
+                    </a>
+
+</div>
+            </div>
+
+        </div>
+    </main>
+
+    <div class="Bola-1-manage-events">
+        <img src="../../../Public/imgs/img-area-adm/bola-1.png" alt="">
+    </div>
+
+    <div class="Bola-2-manage-events">
+        <img src="../../../Public/imgs/img-area-adm/bola-2.png" alt="">
+    </div>
+
+    <div class="Bola-3-manage-events">
+        <img src="../../../Public/imgs/img-area-adm/bola-3.png" alt="">
+    </div>
+
+</body>
+</html>
