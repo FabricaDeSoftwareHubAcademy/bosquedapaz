8b103a8509726c0ad52af4059aed4b25fd7ccc76
gcamargogy <camargogdy@hotmail.com>
2025-02-04 14:40:03 -0400

    validacaoexpatual

--- app/adm/Views/validacao-exp.php
+++ app/adm/Views/validacao-exp.php
@@ -1,132 +1,127 @@
+<?php include "../../../Public/assets/adm/menu-adm.html"?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Validação de Expositores</title>
-    <link rel="stylesheet" href="../../css/css-adm/style-validacao-exp.css">
+    
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../Public/css/css-adm/style-validacao-exp.css">
+
 </head>
 <body>
+    <div class="box-guilherme">
 
-<?php include "../../assets/adm/menu-adm.html"?>
-
-    <div class="box-ajust-guilherme">
-        <div class="box-guilherme">
-            <div class="box-content-guilherme">
-                <h1>Validação de Expositor</h1>
-            </div>
-            <div class="foto-expositor-guilherme">
+        <section class="secao-imagens-guilherme">
+            <div class="imagem-expositor-guilherme">
                 <h1>Foto do Expositor</h1>
-                <img src="../../imgs/img-validacao-expositor/fotoexpositor.png" alt="" class="fotoexp-guilherme">
+                <img src="../../../Public/imgs/img-validacao-expositor/fotoexpositor.png" alt="fotodoexpositor">
             </div>
-            <div class="imagens-content-guilherme">
-                <div class="marca-txt-guilherme">
-                    <h1>Imagens da Marca</h1>
-                </div>
-                <div class="foto-marca1-guilherme">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca.png" alt="">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca2.png" alt="">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca3.png" alt="">
-                </div>
-                <div class="foto-marca2-guilherme">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca4.png" alt="">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca5.png" alt="">
-                    <img src="../../imgs/img-validacao-expositor/fotomarca6.png" alt="">
-                </div>
-                <div class="voltar-guilherme">
-                    <img src="../../imgs/img-validacao-expositor/voltar.svg" alt="">
+
+            <div class="imagens-marca-guilherme">
+                <h1>Imagens da Marca</h1>
+                <div class="imagens-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca.png" alt="fotomarca1" id="i1-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca2.png" alt="fotomarca2" id="i2-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca3.png" alt="fotomarca3" id="i3-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca4.png" alt="fotomarca4" id="i4-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca5.png" alt="fotomarca5" id="i5-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/fotomarca6.png" alt="fotomarca6" id="i6-guilherme">
                 </div>
             </div>
-            <div class="caixa-formulario-guilherme">
-                <div class="formulario-guilherme n1-guilherme">
-                    <label>Nome Completo:</label>
-                    <input type="text" name="" id="" disabled>
-                </div>
+        </section>
 
-                <div class="formulario-guilherme n2-guilherme">
-                    <label>Categoria dos Produtos:</label>
-                    <input type="text" name="" id="" disabled>
+        <section class="secao-informacoes-guilherme">
+            <div class="informacoes-expositor-guilherme">
+                <div class="informacoes-titulo-guilherme">
+                    <h1>Validação de Expositor</h1>
                 </div>
+                <form class="formulario-informacoes-guilherme">
+                    <div>
+                        <label>Nome completo do expositor</label>
+                        <input type="text" name="campo1" id="campo-1" disabled>
 
-                <div class="formulario-guilherme n3-guilherme">
-                    <label>Cidade de Residência:</label>
-                    <input type="text" name="" id="" disabled >
-                </div>
-
-                <div class="formulario-marca-guilherme">
-                    <div class="formulario-guilherme n4-guilherme">
-                        <label>Nome da Marca:</label>
-                        <input type="text" name="" id="" disabled>
+                        <label>Categoria dos produtos</label>
+                        <input type="text" name="campo2" id="campo-2" disabled>
+                        
+                        <label>Precisa de energia? Caso afirmativo, voltagem 110 ou 220? Caso negativo, deixe em branco.</label>
+                        <input type="text" name="campo6" id="campo-6" disabled>
                     </div>
-                    <img src="../../imgs/img-validacao-expositor/logomarca.svg" alt="">
-                </div>
+                    
+                    <div>
+                        <label>WhatsApp para contato</label>
+                        <input type="text" name="campo4" id="campo-4" disabled>
 
-                <div class="caixa-formulario2-guilherme">
-                    <div class="formulario-guilherme n5-guilherme">
-                        <label>WhatsApp para Contato:</label>
-                        <input type="text" name="" id="" disabled>
+                        <label>Cidade de residência</label>
+                        <input type="text" name="campo3" id="campo-3" disabled>
+
+                        <label>Possui trailer, barraca ou foodtruck? Se sim, informe as medidas. Caso negativo, deixe em branco.</label>
+                        <input type="text" name="campo5" id="campo-5" disabled>
                     </div>
+                </form>
+            </div>
 
-                    <div class="formulario-guilherme n6-guilherme">
-                        <label>Possui trailer, barraca ou foodtruck? Se sim, Informe a<br>medida exata dele.</label>
-                        <input type="text" name="" id="" disabled>
-                    </div>
-
-                    <div class="formulario-guilherme n7-guilherme">
-                        <label>Precisa de energia para suas vendas Se sim, qual<br>voltagem? (110) (220)</label>
-                        <input type="number" name="" id="" disabled>
-                    </div>
+            <div class="informacoes-marca-guilherme">
+                <div class="logo-marca-guilherme">
+                    <img src="../../../Public/imgs/img-validacao-expositor/logomarca.svg" alt="logomarca" id="logomarca-guilherme">
                 </div>
-                <div class="linha-guilherme"></div>
+                <form class="formulario-marca-guilherme">
+                    <label>Nome da marca</label>
+                    <input type="text" name="campo7" id="campo-7" disabled>
+                </form>
+            </div>
 
-                <div class="botoes-guilherme">
-                    <a href="#validar-guilherme" class="botao-validar-guilherme">Validar</a>
-                    <a href="#rejeitar-guilherme" class="botao-rejeitar-guilherme">Rejeitar</a>
-                </div>
-                
-                <!-- MODAL VALIDAÇÃOOOOOOOOOOOOO -->
-                <div id="validar-guilherme" class="modal-guilherme">
-                    <div class="modal-content-guilherme">
-                        <h2>Deseja confirmar a validação?</h2>
-                        <div class="modal-botoes-guilherme">
-                            <a href="#validar-sucesso-guilherme" class="botao-confirmar-guilherme">Confirmar</a>
-                            <a href="#" class="botao-voltar-guilherme">Voltar</a>
-                        </div>
-                    </div>
-                </div>
+            <div class="botoes-guilherme">
+                <a href="#modal-validar-guilherme"><button class="botao-validar-guilherme" id="BtnValidar-guilherme">Validar</button></a>
+                <a href="#modal-recusar-guilherme"><button class="botao-recusar-guilherme" id="btnRecusar-guilherme">Recusar</button></a>
+            </div>
+        </section>
 
-                <div id="validar-sucesso-guilherme" class="modal-guilherme">
-                    <div class="modal-content-guilherme">
-                        <h2>Validação confirmada.</h2>
-                        <a href="#" class="botao-fechar-guilherme">Fechar</a>
-                    </div>
-                </div>
-                <!-- MODAL VALIDAÇÃOOOOOOOOOOOOO -->
+    </div>
 
+    <div class="decoracoes-guilherme">
+        <img src="../../../Public/imgs/img-validacao-expositor/voltar.svg" alt="setaVoltar" class="btnVoltar-guilherme">
+        <img src="../../../Public/imgs/img-validacao-expositor/detalhe1.svg" alt="" class="detalhe1">
+        <img src="../../../Public/imgs/img-validacao-expositor/detalhe2.svg" alt="" class="detalhe2">
+        <img src="../../../Public/imgs/img-validacao-expositor/detalhe3.svg" alt="" class="detalhe3">
+    </div>
 
-                <!-- MODAL REIJEIÇÃOOOOOOOOOO -->
-                <div id="rejeitar-guilherme" class="modal-guilherme">
-                    <div class="modal-content-guilherme">
-                        <h2>Deseja confirmar a rejeição?</h2>
-                        <div class="modal-botoes-guilherme">
-                            <a href="#rejeitar-sucesso-guilherme" class="botao-confirmar-guilherme">Confirmar</a>
-                            <a href="#" class="botao-voltar-guilherme">Voltar</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div id="rejeitar-sucesso-guilherme" class="modal-guilherme">
-                    <div class="modal-content-guilherme">
-                        <h2>Rejeição confirmada.</h2>
-                        <a href="#" class="botao-fechar-guilherme">Fechar</a>
-                    </div>
-                </div>
-                <!-- MODAL REIJEIÇÃOOOOOOOOOO -->
-
+    <div class="modal-validar-guilherme" id="modal-validar-guilherme">
+        <div class="modal-content-guilherme">
+            <h1>Deseja validar o expositor?</h1>
+            <div class="modal-botoes-guilherme">
+                <a href="#modal-validar-sucesso-guilherme"><button class="btn-modal-guilherme btn-validar-guilherme">Validar</button></a>
+                <a href="#"><button class="btn-modal-guilherme btn-voltar-guilherme">Voltar</button></a>
             </div>
         </div>
     </div>
 
-    <script src="../js/main.js"></script>
+    <div class="modal-recusar-guilherme" id="modal-recusar-guilherme">
+        <div class="modal-content-guilherme">
+            <h1>Deseja recusar o expositor?</h1>
+            <div class="modal-botoes-guilherme">
+                <a href="#modal-recusar-sucesso-guilherme"><button class="btn-modal-guilherme btn-recusar-guilherme">Recusar</button></a>
+                <a href="#"><button class="btn-modal-guilherme btn-voltar-guilherme">Voltar</button></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal-validar-sucesso-guilherme" id="modal-validar-sucesso-guilherme">
+        <div class="modal-content-guilherme">
+            <h1>Validado com sucesso!</h1>
+            <div class="modal-botoes-guilherme">
+                <a href="#"><button class="btn-modal-guilherme btn-ok-guilherme">Ok</button></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal-recusar-sucesso-guilherme" id="modal-recusar-sucesso-guilherme">
+        <div class="modal-content-guilherme">
+            <h1>Recusado com sucesso!</h1>
+            <div class="modal-botoes-guilherme">
+                <a href="#"><button class="btn-modal-guilherme btn-ok-guilherme">Ok</button></a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
