3cb6e9f17ffa1c372f911fd7387e8bb65cc41480
Teodororibas <teodororibas@icloud.com>
2025-02-05 15:14:18 -0400

    parceiros

--- app/adm/Views/tela-parceiros.php
+++ app/adm/Views/tela-parceiros.php
@@ -12,41 +12,213 @@
     </head>
 
     <body>
+    <header class="cabecalho">
         <main class="principal">
             <div class="box">
-                <h1 class="tela-titulo">ÁREA PARCEIROS</h1>
+                <h2>PARCEIROS</h2>
+                <div class="container">
+                    <div class="search-bar">
+                      <label for="status">Procurar</label>
+                      <input type="text" id="status" placeholder="Expositor" />
+                      <button class="search-button">BUSCAR</button>
+                    </div>
+                
+                    <table class="collaborators-table">
+                      <thead>
+                        <tr>
+                          <th class="usuario-col">Usuário</th>
+                          <th>Nome</th>
+                          <th class="email-col">E-mail</th>
+                          <th class="fone-col">Telefone</th>
+                          <th class="barraca-col">N. Barraca</th>
+                          <th>Status</th>
+                          <th>Editar</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <!-- Rascunho  -->
+                        <tr>
+                          <td class="usuario-col">01</td>
+                          <td>Carla  Costa</td>
+                          <td class="email-col">carla.costa123@gmail.com</td>
+                          <td class="fone-col">(67) 98123-4567</td>
+                          <td class="barraca-col">6</td>
+                          <td><button class="status active">Ativo</button></td>
+                          <td>
+                              <a href="editar-adm.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="usuario-col">02</td>
+                          <td>Juan Quintela</td>
+                          <td class="email-col">juan.quintela987@gmail.com</td>
+                          <td class="fone-col">(67) 98234-5678</td>
+                          <td class="barraca-col">12</td>
+                          <td><button class="status inactive">Inativo</button></td>
+                          <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="usuario-col">03</td>
+                          <td>Julia Souza</td>
+                          <td class="email-col">julia.souza456@gmail.com</td>
+                          <td class="fone-col">(67) 98945-6789</td>
+                          <td class="barraca-col">10</td>
 
-                <div class="todo-conteudos">
-                    <div class="div-parceiros parceiros-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 1" class="img-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 2" class="img-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 3" class="img-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 4" class="img-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 5" class="img-pref">
-                        <img src="../../../Public/imgs/img-parceiros.css/pref-cg.png" alt="Imagem de parceiro 6" class="img-pref">
-                    </div>
-                                     
-                    <div class="geral">
-                        <img src="../../../Public/imgs/img-parceiros.css/linha-central.png" alt="linha-central" class="linha-central">
-                        <a href="../../../app/adm/Views/cadastro-parceiros.php">
-                            <div class="button-cadastrar">
-                                <button class="button">CADASTRAR</button>
-                            </div>
-                        </a>
-                        <img src="../../../Public/imgs/img-parceiros.css/foto-parceiros.png" alt="imagem-parceiros" class="img-g">
-                    </div>
-                    
-                </div>
-                    
+                          <td><button class="status active">Ativo</button></td>
+                          <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                          </td>
+                        </tr>
+                        <tr>
+                            <td class="usuario-col">13</td>
+                            <td>Pedro Alves</td>
+                            <td class="email-col">pedro.alves789@gmail.com</td>
+                            <td class="fone-col">(67) 98845-6789</td>
+                          <td class="barraca-col">6</td>
+
+                            <td><button class="status inactive">Inativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">05</td>
+                            <td>Nara Helena</td>
+                            <td class="email-col">nara.helena126@gmail.com</td>
+                            <td class="fone-col">(67) 98345-6789</td>
+                          <td class="barraca-col">45</td>
+
+                            <td><button class="status active">Ativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">06</td>
+                            <td>Fernanda Santos</td>
+                            <td class="email-col">fernanda.santos126@gmail.com</td>
+                            <td class="fone-col">(67) 97345-6623</td>
+                          <td class="barraca-col">24</td>
+
+                            <td><button class="status active">Ativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">07</td>
+                            <td>Emanuelle Valadares</td>
+                            <td class="email-col">manu.vala777@gmail.com</td>
+                            <td class="fone-col">(67) 98885-6888</td>
+                          <td class="barraca-col">26</td>
+
+                            <td><button class="status inactive">Inativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">08</td>
+                            <td>Kauan Ribeiro</td>
+                            <td class="email-col">kauan.ribeiro753@gmail.com</td>
+                            <td class="fone-col">(67) 99942-1110</td>
+                          <td class="barraca-col">23</td>
+
+                            <td><button class="status active">Ativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">09</td>
+                            <td>Vini Count</td>
+                            <td class="email-col">count.vini99@gmail.com</td>
+                            <td class="fone-col">(67) 99210-2566</td>
+                          <td class="barraca-col">75</td>
+
+                            <td><button class="status inactive">Inativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">10</td>
+                            <td>Isabela Oliveira</td>
+                            <td class="email-col">isa.bela555@gmail.com</td>
+                            <td class="fone-col">(67) 96841-5517</td>
+                          <td class="barraca-col">2</td>
+
+                            <td><button class="status active">Ativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">11</td>
+                            <td>Kelvin Bach</td>
+                            <td class="email-col">kelvin.bach0208@gmail.com</td>
+                            <td class="fone-col">(67) 90208-5623</td>
+                          <td class="barraca-col">44</td>
+
+                            <td><button class="status inactive">Inativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>
+                        <tr>
+                            <td class="usuario-col">12</td>
+                            <td>Paulo Henrique</td>
+                            <td class="email-col">paulo.henrique33@gmail.com</td>
+                            <td class="fone-col">(67) 98345-6789</td>
+                          <td class="barraca-col">7</td>
+
+                            <td><button class="status active">Ativo</button></td>
+                            <td>
+                              <a href="edicao-expositor.html">
+                                  <i class="fa-solid fa-pen-to-square"></i>
+                              </a>
+                            </td>
+                          </tr>  
+                      </tbody>
+                    </table>
+            <div class="btns">
+                <a href="Area-Adm.php" class="voltar">
+                <img src="../../../Public/imgs/img-listar-colaboradores/btn-voltar.png" alt="Botão de voltar" class="btn-voltar">
+                </a>
+            </div>  
             </div>
         </main>
 
-        <div class="bolas-fundo">
-            <img src="../../../Public/imgs/imagens-bolas/bola-verde1.png" alt="" class="bola-verde1">
-            <img src="../../../Public/imgs/imagens-bolas/bola-verde2.png" alt="" class="bola-verde2">
-            <img src="../../../Public/imgs/imagens-bolas/Elemento3.ElipseRosa.png" alt="" class="bola-rosa">
-        </div>
+            <div class="bolas-fundo">
+                <img class="bola-azul1"   src="../img/Elemento1.FolhaAzul.png" alt="">
+                <img class="bola-azul2"   src="../img/Elemento2.FolhaAzul.png" alt="">
+                <img class="bola-azul3"   src="../img/Elemento3.ElipseAzul.png" alt="">
+            </div>
 
-        <script src="../js/main.js"></script>
-    </body>
+    <script src="../../../Public/js//js-adm/status-colaborador.js"></script>
+</body>
 </html>
