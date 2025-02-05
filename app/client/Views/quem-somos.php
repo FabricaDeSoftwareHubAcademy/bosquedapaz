7cd0d5de2971cb76001e89daf9cb174114206305
Samuel1204-lab <samukarodrigues07@gmail.com>
2025-02-05 16:16:45 -0400

    quem-somos rodape

--- app/client/Views/quem-somos.php
+++ app/client/Views/quem-somos.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../Public/css/css-home/quem-somos.css">
-    <link rel="stylesheet" href="../../../Public/css/css-home/rodape.css">
+    
     <title>Quem Somos</title>
 </head>
 <body>
