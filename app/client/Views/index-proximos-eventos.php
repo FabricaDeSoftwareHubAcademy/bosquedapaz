3d18088932d0ed17328752fc5ed4e741c76058a1
Matheus Manja <matheusmanja7@gmail.com>
2025-02-06 14:19:47 -0400

    comit atualização tela proximos eventos

--- app/client/Views/index-proximos-eventos.php
+++ app/client/Views/index-proximos-eventos.php
@@ -113,9 +113,9 @@
 
         <!-- Box Peincipal -->
         <div class="box-principal-ProEv">
-            <!-- <div class="area-setaProEv">
-                <a href="index.php"><img src="../../../Public/imgs/imgs-lista-de-espera/seta-lispe.png" alt=""></a>
-            </div> -->
+            <div class="area-setaProEv">
+                <a href="../../../index.php"><img src="../../../Public/imgs/imgs-lista-de-espera/seta-lispe.png" alt=""></a>
+            </div>
            
             <!-- Area das Informações do Evento -->
             <div class="area-info-evento-ProEv">
