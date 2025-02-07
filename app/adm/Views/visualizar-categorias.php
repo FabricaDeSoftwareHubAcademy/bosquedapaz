ac258abc9b8441316d310c518821000d8821d413
EnzoArimura15 <enzo.arimura@gmail.com>
2025-02-07 14:35:34 -0400

    correção

--- app/adm/Views/visualizar-categorias.php
+++ app/adm/Views/visualizar-categorias.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Categorias</title>
-        <link rel="stylesheet" href="../../../Public/css/css-adm/style-listar-categoria.css" >
+        <link rel="stylesheet" href="../../../Public/css/css-adm/style-visualizar-categoria.css" >
         <link rel="stylesheet" href="../Public/assets/adm/menu-adm.html">
     </head>
     
@@ -23,61 +23,61 @@
         <div class="box-item">
             <div class="item">
                 <div class="bolota" id="b1">
-                    <img src="../../../Public/assets/icons/icones-categorias/Paint Brush (1).png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Pincel.png" alt="" class="icon-item">
                 </div>
                 <p>Artesanato</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b2">
-                    <img src="../../../Public/assets/icons/icones-categorias/Parchment.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Papiro.png" alt="" class="icon-item">
                 </div>
                 <p>Antiguidade</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b3">
-                    <img src="../../../Public/assets/icons/icones-categorias/NFT Collection.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Quadrados.png" alt="" class="icon-item">
                 </div>
                 <p>Colecionismo</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b4">
-                    <img src="../../../Public/assets/icons/icones-categorias/Vial.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Frasco.png" alt="" class="icon-item">
                 </div>
                 <p>Cosmetologia</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b5">
-                    <img src="../../../Public/assets/icons/icones-categorias/Real Food for Meals.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Frasco.png" alt="" class="icon-item">
                 </div>
                 <p>Gastronomia</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b6">
-                    <img src="../../../Public/assets/icons/icones-categorias/Books.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Livros.png" alt="" class="icon-item">
                 </div>
                 <p>Literatura</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b7">
-                    <img src="../../../Public/assets/icons/icones-categorias/Scissors.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Tesoura.png" alt="" class="icon-item">
                 </div>
                 <p>Moda Autoral</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b8">
-                    <img src="../../../Public/assets/icons/icones-categorias/Potted Plant.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Planta.png" alt="" class="icon-item">
                 </div>
                 <p>Plantas</p>
             </div>
             <div class="item">
                 <div class="bolota" id="b9">
-                    <img src="../../../Public/assets/icons/icones-categorias/Recycle.png" alt="" class="icon-item">
+                    <img src="../../../Public/assets/icons/icones-categorias/Reciclar.png" alt="" class="icon-item">
                 </div>
                 <p>Sustentabilidade</p>
             </div>
                 <a href="cadastro-categoria.php" class="item">
         <div class="bolota" id="b10">
-            <img src="../../../Public/assets/icons/icones-categorias/plus-circle.png" alt="" class="icon-item">
+            <img src="../../../Public/assets/icons/icones-categorias/Circulo-mais.png" alt="" class="icon-item">
         </div>
         <p>Nova Categoria</p>
     </a>
