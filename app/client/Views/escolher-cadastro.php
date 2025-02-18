8e5fc1f22f23de80c11ca254db4e432ef7d0de17
Matheus Manja <matheusmanja7@gmail.com>
2025-02-18 16:08:41 -0400

    comit atualizacao da task 63

--- app/client/Views/escolher-cadastro.php
+++ app/client/Views/escolher-cadastro.php
@@ -6,66 +6,53 @@
     <title>Cadastro de expositor</title>
     <link rel="stylesheet" href="../../../Public/css/css-home/escolher-cadastro.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
-
 </head>
 <body>
     <?php include "../../../Public/assets/home/menu-home.html"; ?>
-
     <!-- inicio da parte principal da pagina -->
     <main class="principal">
-        
         <!-- box principal -->
         <div class="box">
             <div id="linha-vertical-ec"></div>
             <div class="area-infs-ec">
                 <h1>Escolher Cadastro</h1>
-
-                <div class="area-inputs-ec">
-                    <div class="total-input-ec">
-                        <div class="icon-input-ec">
-                            <i id="icon-ec" class="bi bi-shop-window"></i>
-                        </div>
-                        <div class="text-input-ec">
-                            <p>Cadastro de Espoxitor</p>
-                        </div>
+                <a href="cadastro-expositor.php" class="area-total-button">
+                    <div class="icon-input-ec">
+                        <i id="icon-ec" class="bi bi-shop-window"></i>
                     </div>
-
-                    <div class="total-input-ec">
-                        <div class="icon-input-ec">
-                            <i id="icon-ec" class="bi bi-person-plus"></i>
-                        </div>
-                        <div class="text-input-ec">
-                            <p>Cadastro de Espoxitor Kids</p>
-                        </div>
+                    <div class="text-input-ec">
+                        <p>Cadastro de Expositor</p>
                     </div>
-
-                    <div class="total-input-ec">
-                        <div class="icon-input-ec">
-                            <i id="icon-ec" class="bi bi-music-note-list"></i>
-                        </div>
-                        <div class="text-input-ec">
-                            <p>Cadastro de Artista</p>
-                        </div>
+                </a>
+                <a href="cadastro-expositor-kids.php" class="area-total-button">
+                    <div class="icon-input-ec">
+                        <i id="icon-ec" class="bi bi-person-plus"></i>
                     </div>
-                </div>
+                    <div class="text-input-ec">
+                        <p>Cadastro de Expositor</p>
+                    </div>
+                </a>
+                <a href="cadastro-artista.php" class="area-total-button">
+                    <div class="icon-input-ec">
+                        <i id="icon-ec" class="bi bi-music-note-list"></i>
+                    </div>
+                    <div class="text-input-ec">
+                        <p>Cadastro de Expositor</p>
+                    </div>
+                </a>
             </div>
-
-
             <div class="area-img-ec">
                 <img src="../../../Public/imgs/img-escolher-cadastro/img-ec.svg" alt="">
             </div>
         </div>
     </main>
-
     <!-- bolas de fundo -->
     <div class="bolas-fundo">
         <img src="../../../Public/imgs/imagens-bolas/bola-verde1.png" alt="Bola Fundo 1" class="bola-verde1">
         <img src="../../../Public/imgs/imagens-bolas/bola-verde2.png" alt="Bola Fundo 2" class="bola-verde2">
         <img src="../../../Public/imgs/imagens-bolas/bola-rosa.png" alt="Bola Fundo 3" class="bola-rosa">
     </div>
-
     <!-- link do JavaScript -->
     <script src="../../../Public/js/js-menu/js-menu.js"></script>
-
 </body>
 </html>
