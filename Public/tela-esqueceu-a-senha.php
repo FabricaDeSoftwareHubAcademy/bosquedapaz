0056034dede59b105fa494e0c32a7f22adf3bf77
TiagoSnts <tsc.3602006@gmail.com>
2025-02-21 14:27:43 -0400

    atualizações
    responsividade e uma img nova

--- Public/tela-esqueceu-a-senha.php
+++ Public/tela-esqueceu-a-senha.php
@@ -6,29 +6,28 @@
     <title>Prévia Tela Esqueceu a Senha</title>
     <link rel="stylesheet" href="../Public/css/esqueceu-a-senha-recsenha.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="shortcut icon" href="./assets/icons/folha.ico" >
 </head>
 <body class="body-recsenha">
     <main>
         <section class="section-recsenha">
             <div class="img-superior-direita-recsenha">
-                <img src="../Imagens/imagem-superior-direito.svg" alt="">
+                <img class="imgg-superior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-direito.svg" alt="">
             </div>
             <div class="img-superior-esquerda-recsenha">
-                <img src="../Imagens/imagem-superior-esquerdo.svg" alt="">
+                <img src="../Public/imgs/imagens-bolas/imagem-superior-esquerdo.svg" alt="">
             </div>
             <div class="img-inferior-direita-recsenha">
-                <img src="../Imagens/imagem-inferior-direito.svg" alt="">
+                <img src="../Public/imgs/imagens-bolas/imagem-inferior-direito.svg" alt="">
             </div>
             <div class="img-inferior-esquerda-recsenha">
-                <img src="../Imagens/imagem-inferior-esquerdo.svg" alt="">
+                <img class="imgg-inferior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-esquerdo.svg" alt="">
             </div>
  
             <div class="box-recsenha">
  
                 <div class="botao-voltar">
-                    <a href="tela-login.html" class="volte">
-                        <img src="../Imagens/arrow-circle-left.svg" alt="">
+                    <a href="tela-login.php" class="volte">
+                        <img src="../Public/imgs/img-login/arrow-circle-left.svg" alt="">
                     </a>
                 </div>
                
@@ -51,7 +50,7 @@
                     </div>
                     <div class="div-botoes-recsenha">
                         <div class="div-botao-cancelar-recsenha">
-                            <a class="botao-cancelar-recsenha" href="tela-login.html">Cancelar</a>
+                            <a class="botao-cancelar-recsenha" href="tela-login.php">Cancelar</a>
                             <!-- <button class="botao-cancelar-recsenha">Cancelar</button> -->
                         </div>
                         <div class="div-botao-enviar-recsenha">
@@ -66,7 +65,7 @@
                     <h1 class="text-h1-recsenha">REDEFINIÇÃO DE SENHA</h1>
  
                     <div class="img-recsenha">
-                        <img class="img-recsenha-svg-recsenha" src="../Imagens/message-sent.svg" alt="">
+                        <img class="img-recsenha-svg-recsenha" src="../Public/imgs/img-login/message-sent.svg" alt="">
                     </div>
                 </div>
             </div>
