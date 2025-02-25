e4f801f7ccefeec1f081e739373751fac2889fd6
Raquel Leirião Marques <raquelmarques2503@gmail.com>
2025-02-25 14:05:19 -0400

    .

--- app/adm/Views/Area-Adm.php
+++ app/adm/Views/Area-Adm.php
@@ -6,6 +6,7 @@
     <title>Área Administrativa</title>
     <link rel="stylesheet" href="../../../Public/css/menu-adm.css">
     <link rel="stylesheet" href="../../../Public/css/css-adm/Area-Adm.css">
+    <link rel="icon" href="../../imgs/img-home/logo.png" type="image/x-icon">
 </head>
 <body>
 
