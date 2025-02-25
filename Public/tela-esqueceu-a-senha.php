25a1c87ef82f6b601077de21393bd853f9cfc9f6
TiagoSnts <tsc.3602006@gmail.com>
2025-02-25 17:29:26 -0400

    atualização
    responsividade principalmente

--- Public/tela-esqueceu-a-senha.php
+++ Public/tela-esqueceu-a-senha.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,32 +8,22 @@
     <link rel="stylesheet" href="../Public/css/esqueceu-a-senha-recsenha.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
+
 <body class="body-recsenha">
     <main>
         <section class="section-recsenha">
-            <div class="img-superior-direita-recsenha">
-                <img class="imgg-superior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-direito.svg" alt="">
-            </div>
-            <div class="img-superior-esquerda-recsenha">
-                <img class="imgg-superior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-esquerdo.svg" alt="">
-            </div>
-            <div class="img-inferior-direita-recsenha">
-                <img class="imgg-inferior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-direito.svg" alt="">
-            </div>
-            <div class="img-inferior-esquerda-recsenha">
-                <img class="imgg-inferior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-esquerdo.svg" alt="">
-            </div>
- 
+            <img class="imgg-superior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-direito.svg" alt="">
+            <img class="imgg-superior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-esquerdo.svg" alt="">
+            <img class="imgg-inferior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-direito.svg" alt="">
+            <img class="imgg-inferior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-esquerdo.svg" alt="">
+
             <div class="box-recsenha">
-               
+
                 <div id="linha-bet-recsenha"></div>
                 <div class="area-recsenha">
                     <div class="center-recsenha">
-                        <div class="div-redefinicao">
-                            <p>REDEFINIÇÃO DE SENHA</p>
-                        </div>
                         <div class="div-text-recsenha">
-                            <P>Digite seu email abaixo para redefinir sua senha</P>
+                            <P class="text-rec">Digite seu email abaixo para redefinir sua senha</P>
                         </div>
                         <form action="#" class="form-recsenha">
                             <label class="text-endereco-email">Endereço de e-mail:</label>
@@ -45,24 +36,18 @@
                     <div class="div-botoes-recsenha">
                         <div class="div-botao-cancelar-recsenha">
                             <a class="botao-cancelar-recsenha" href="tela-login.php">Cancelar</a>
-                            <!-- <button class="botao-cancelar-recsenha">Cancelar</button> -->
                         </div>
                         <div class="div-botao-enviar-recsenha">
                             <button class="botao-enviar-recsenha open-modal" data-modal="modal-recsenha">Enviar</button>
                         </div>
-                        <!-- <dialog class="dialog-recsenha" id="modal-recsenha">
-                            <button class="close-modal" data-modal="modal-recsenha"></button>
-                        </dialog> -->
                     </div>
-                    <div class="botao-voltar">
                     <a href="tela-login.php" class="volte">
-                        <img src="../Public/imgs/img-login/arrow-circle-left.svg" alt="">
+                        <img class="img-circle" src="../Public/imgs/img-login/arrow-circle-left.svg" alt="">
                     </a>
-                </div>
                 </div>
                 <div class="area-img-recsenha">
                     <h1 class="text-h1-recsenha">REDEFINIÇÃO DE SENHA</h1>
- 
+
                     <div class="img-recsenha">
                         <img class="img-recsenha-svg-recsenha" src="../Public/imgs/img-login/message-sent.svg" alt="">
                     </div>
@@ -71,4 +56,5 @@
         </section>
     </main>
 </body>
+
 </html>
