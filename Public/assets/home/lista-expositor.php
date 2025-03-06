509d0709cce6a0833d05a8aa3acd51168f333431
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-03-06 16:02:57 -0400

    hover dos cards completos

--- Public/assets/home/lista-expositor.php
+++ Public/assets/home/lista-expositor.php
@@ -8,26 +8,78 @@
         </div>
         <p class="para-title">Veja como são nossos expositores e venha explorar, apoiar e compartilhar uma experiência inesquecível conosco!</p>
     </div>
-
     <div class="listar-expositor">
         <div class="cards-perfis">
             <div class="card-per-expo">
-                <div class="head-card"></div>
-                <div class="body-card"></div>
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/akj-prime.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
             </div>
 
             <div class="card-per-expo">
-                <div class="head-card"></div>
-                <div class="body-card"></div>
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/decorart.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
             </div>
 
             <div class="card-per-expo">
-                <div class="head-card"></div>
-                <div class="body-card"></div>
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/cake-pet.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
             </div>
             <div class="card-per-expo">
-                <div class="head-card"></div>
-                <div class="body-card"></div>
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/retalhos-e-chica.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
+            </div>
+        </div>
+        <div class="cards-perfis">
+            <div class="card-per-expo">
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/decorart.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
+            </div>
+
+            <div class="card-per-expo">
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/cake-pet.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
+            </div>
+
+            <div class="card-per-expo">
+                <div class="head-card">
+                    <img src="../../../Public/imgs/img-home/retalhos-e-chica.png" alt="" class="img-perfil-expo">
+                </div>
+                <div class="body-card">
+                
+                </div>
+            </div>
+            <div class="card-per-expo">
+                <div class="head-card head-seja-expo">
+                    <h3 class="seja-expo">Seja um Expositor</h3>
+                </div>
+                <div class="body-card">
+                    <p class="para-seja-expo">Junte-se à nossa comunidade de expositores e ganhe visibilidade para a sua marca! Se torne um expositor clicando no botão abaixo.</p>
+                    <button class="btn-seja-expo"><a href="" class="link-edital">Clique Aqui</a></button>
+                </div>
             </div>
         </div>
     </div>
