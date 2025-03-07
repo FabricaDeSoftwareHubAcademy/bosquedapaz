041c833201741ff351dff06f6c2c59a1dec5dca7
Matheus Manja <matheusmanja7@gmail.com>
2025-03-07 17:31:34 -0400

    comit Atualização da responsividade da tela proximos eventos

--- Public/js/js-modais/modal-proximo-evento.js
+++ Public/js/js-modais/modal-proximo-evento.js
@@ -1,29 +1,34 @@
 const openButtons = document.querySelectorAll('.open-modal');
-const overlay = document.getElementById('overlay'); // Selecione o elemento da sobreposição
-const overlaay = document.getElementById('overlaay'); // Selecione o elemento da sobreposição
+const closeButtons = document.querySelectorAll('.close-modal');
+const overlay = document.getElementById('overlay'); 
+const overlaay = document.getElementById('overlaay');
 
 openButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId);
         
-        // Mostra o modal e a sobreposição
         modal.showModal();
-        overlay.style.display = 'block'; // Exibe a sobreposição
-        overlaay.style.display = 'block'; // Exibe a sobreposição
-    })
-})
+        overlay.style.display = 'block';
+        overlaay.style.display = 'block';
+        overlayy.style.display = 'block';
 
-const closeButtons = document.querySelectorAll('.close-modal');
+        // Impede o scroll
+        document.body.style.overflow = 'hidden';
+    });
+});
 
 closeButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId);
         
-        // Fecha o modal e esconde a sobreposição
         modal.close();
-        overlay.style.display = 'none'; // Esconde a sobreposição
-        overlaay.style.display = 'none'; // Esconde a sobreposição
-    })
-})
+        overlay.style.display = 'none';
+        overlaay.style.display = 'none';
+        overlayy.style.display = 'none';
+
+        // Restaura o scroll
+        document.body.style.overflow = '';
+    });
+});
