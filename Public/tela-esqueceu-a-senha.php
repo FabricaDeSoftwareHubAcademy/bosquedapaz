ca852b554207ef41cac20f44d96482a121253a8f
TiagoSnts <tsc.3602006@gmail.com>
2025-03-14 17:35:13 -0400

    atualizações
    reescrita completa da tela de recuperação de senha e começo de novo css, leves alterações no css da tela de login

--- Public/tela-esqueceu-a-senha.php
+++ Public/tela-esqueceu-a-senha.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -10,6 +10,7 @@
 <body class="body-recsenha">
     <main>
         <section class="section-recsenha">
+
             <div class="img-superior-direita-recsenha">
                 <img class="imgg-superior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-direito.svg" alt="">
             </div>
@@ -24,8 +25,14 @@
             </div>
  
             <div class="box-recsenha">
-               
                 <div id="linha-bet-recsenha"></div>
+                 
+                <div class="botao-voltar">
+                    <a href="tela-login.php" class="volte">
+                        <img src="../Public/imgs/img-login/arrow-circle-left.svg" alt="">
+                    </a>
+                </div>
+
                 <div class="area-recsenha">
                     <div class="center-recsenha">
                         <div class="div-redefinicao">
@@ -45,20 +52,11 @@
                     <div class="div-botoes-recsenha">
                         <div class="div-botao-cancelar-recsenha">
                             <a class="botao-cancelar-recsenha" href="tela-login.php">Cancelar</a>
-                            <!-- <button class="botao-cancelar-recsenha">Cancelar</button> -->
                         </div>
                         <div class="div-botao-enviar-recsenha">
                             <button class="botao-enviar-recsenha open-modal" data-modal="modal-recsenha">Enviar</button>
                         </div>
-                        <!-- <dialog class="dialog-recsenha" id="modal-recsenha">
-                            <button class="close-modal" data-modal="modal-recsenha"></button>
-                        </dialog> -->
                     </div>
-                    <div class="botao-voltar">
-                    <a href="tela-login.php" class="volte">
-                        <img src="../Public/imgs/img-login/arrow-circle-left.svg" alt="">
-                    </a>
-                </div>
                 </div>
                 <div class="area-img-recsenha">
                     <h1 class="text-h1-recsenha">REDEFINIÇÃO DE SENHA</h1>
@@ -67,6 +65,66 @@
                         <img class="img-recsenha-svg-recsenha" src="../Public/imgs/img-login/message-sent.svg" alt="">
                     </div>
                 </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperação de senha</title>
+    <link rel="stylesheet" href="../Public/css/style-esqueceu-a-senha-recsenha.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+<body class="body-recsenha">
+    <main>
+        <section class="section-recsenha">
+
+            <img class="imgg-superior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-direito.svg" alt="">
+        
+            <img class="imgg-superior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-superior-esquerdo.svg" alt="">
+            
+            <img class="imgg-inferior-direita-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-direito.svg" alt="">
+            
+            <img class="imgg-inferior-esquerda-recsenha" src="../Public/imgs/imagens-bolas/imagem-inferior-esquerdo.svg" alt="">
+            
+            <div class="box-recsenha">
+                
+                <a href="tela-login.php" class="volte"><img class="img-voltar" src="../Public/imgs/img-login/arrow-circle-left.svg" alt=""></a>
+                
+                <!-- Form -->
+                <div class="form-recsenha">
+                    
+                    <form action="#" class="forms-recsenha">
+                        <p>Digite seu email abaixo para redefinir sua senha</p>
+                        <label>Email</label>
+                        <div class="area-input-recsenha">
+                            <i class="bi bi-envelope"></i>
+                            <input class="input-recsenha" type="email" name="email" id="email" placeholder="Digite seu email" required>
+                        </div>
+                    </form>
+                    
+                    <div class="botoes">
+                        <a href="tela-login.php" class="botao-cancelar">cancelar</a>
+                        <a href="" class="botao-redefinir">redefinir</a>
+                    </div>
+                    
+                </div>
+
+                <div id="linha-bet-recsenha"></div>
+
+                <!-- area-img -->
+                <div class="area-img-recsenha">
+                    <h1 class="title-recsenha">REDEFINIÇÃO DE SENHA</h1>
+                    <div class="img-style">
+                        <img src="imgs/img-login/message-sent.svg" alt="" class="img-letter">
+                    </div>
+                </div>
+
             </div>
         </section>
     </main>
