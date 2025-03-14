38d7fc56da6004b9ffcc81bc4654d4d78f9b905e
Gabriel-A77 <galvesdesouza386@gmail.com>
2025-03-14 14:40:39 -0400

    atualiza

--- app/client/Views/utilidades-publicas.php
+++ app/client/Views/utilidades-publicas.php
@@ -6,10 +6,10 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../../Public/css/css-home/utilidades-publicas.css">
+        <link rel="stylesheet" href="../../../Public/css/css-home/style-utilidades-publicas.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- <link rel="stylesheet" href="../../../bosquedapaz/css/styles-home/menu.css"> -->
-        <link rel="stylesheet" href="../../../Public/css/menu-home.css">
+        <link rel="stylesheet" href="../../../Public/css/css-home/style-menu.css">
         <title>Gerenciar Eventos</title>
     </head>
     
