1d567cc9cb49ed8a2d8b161c1afb1bba97567511
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-03-17 14:10:12 -0400

    mudança title

--- app/client/Views/todos-expositores.php
+++ app/client/Views/todos-expositores.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listas de Expositores</title>
+    <title>Feira Bosque da Paz</title>
     <link rel="stylesheet" href="../../../Public/css/css-home/style-lista-expositor.css">
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico" >
 </head>
