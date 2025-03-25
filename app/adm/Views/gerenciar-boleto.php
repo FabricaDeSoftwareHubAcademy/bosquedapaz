d53284153335cf261883c9f8ca9d6e5cf9cff11c
gcamargogy <camargogdy@hotmail.com>
2025-03-25 14:25:20 -0400

    fixed 1

--- app/adm/Views/gerenciar-boleto.php
+++ app/adm/Views/gerenciar-boleto.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciamento de Boletos </title>
-    <link rel="stylesheet" href="../../../Public/css/css-adm/g_boleto.css">
+    <link rel="stylesheet" href="../../../Public/css/css-adm/style-gerenciar-boletos.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
 </head>
