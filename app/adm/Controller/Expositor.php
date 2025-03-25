54dd445b7136f757163e886457f6fe8994b80e41
Maykelly Castro <cezariomaykelly@gmail.com>
2025-03-25 17:32:40 -0400

    update

---
+++ app/adm/Controller/Expositor.php
@@ -0,0 +1,11 @@
+<?php
+
+// STRING = texto
+// INT = numeros inteiros
+// FLOAT / DOUBLE = numeros com virgula ou ponto
+// BOOL = verdadeiro ou falso
+
+class Expositor {
+    public int $id_expositor;
+    
+}
