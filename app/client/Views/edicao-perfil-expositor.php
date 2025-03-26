8bb111fe394955f8c1938186fc95a16fa6fa021e
Matheus Manja <matheusmanja7@gmail.com>
2025-03-26 16:41:39 -0400

    comit atualizações

---
+++ app/client/Views/edicao-perfil-expositor.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../Public/css/css-home/edicao-perfil-expositor.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "../../../Public/assets/adm/menu-adm.html" ?>
+
+    <section class="principal">
+        <div class="img1"><img src="../../../Public/imgs/imgs-edicao-perfil-expo/img1.png" alt=""></div>
+        <div class="img2"><img src="../../../Public/imgs/imgs-edicao-perfil-expo/img2.png" alt=""></div>
+        <div class="img3"><img src="../../../Public/imgs/imgs-edicao-perfil-expo/img3.png" alt=""></div>
+
+        <div class="box-principal">
+            <div class="area-h1"><h1>Edição Perfil Expositor</h1></div>
+            
+
+            <div class="area-infs">
+                <div class="lado-esquerdo"></div>
+                <div class="lado-direito">
+                    <div class="area-text">
+                        <h1>Cake Page</h1>
+                    </div>
+
+
+                    <div class="area-cat-num"></div>
+
+
+                    <div class="area-inf-corR"></div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    
+    
+
+</body>
+</html>
