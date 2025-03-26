02c8cf1ef87b19f6d33ab89aec596125df76b3b1
Maykelly Castro <cezariomaykelly@gmail.com>
2025-03-25 22:14:28 -0400

    Update Expositor.php

--- app/adm/Controller/Expositor.php
+++ app/adm/Controller/Expositor.php
@@ -7,5 +7,15 @@
 
 class Expositor {
     public int $id_expositor;
-    
+    public int $id_pessoa;
+    public int $id_categoria;
+    public int $id_imagem;
+    public string $nome_marca;
+    public string $dimensoes;
+    public int $num_barraca;
+    public string $energia;
+    public int $contato2;
+    public string $descricao;
+    public string $metodos_pgto;
+    public string $cor_rua;
 }
