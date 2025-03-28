44c71be95c62e73d9ae864187e45c4a0bdd1aab6
Matheus Manja <matheusmanja7@gmail.com>
2025-03-28 14:36:18 -0400

    comit atualiza

--- app/client/Views/edicao-perfil-expositor.php
+++ app/client/Views/edicao-perfil-expositor.php
@@ -16,118 +16,77 @@
         <div class="img3"><img src="../../../Public/imgs/imgs-edicao-perfil-expo/img3.png" alt=""></div>
 
         <div class="box-principal">
-            <div class="area-h1"><h1>Edição Perfil Expositor</h1></div>
-            
-
+            <div class="area-h1">
+                <h1>Edição Perfil Expositor</h1>
+            </div>
             <div class="area-infs">
-                <div class="lado-esquerdo">
-                    <div class="area-logo">
-                        <label for="file" class="uploads" id="input">
-                            <input type="file" name="imagem" id="imagem" class="img-input" multiple>
-                            <img src="../../../Public/imgs/imgs-edicao-perfil-expo/logo-marca.png" alt="">
-                        </label>
-                    </div>
-                    <div class="text-produtos"></div>
-                    <div class="area-imgs"></div>
-                </div>
-
-
-                <div class="lado-direito">
-                    <div class="area-text">
-                        <h1 class="title">Play Artesanato</h1>
-                        <h3 class="sobre">Sobre a Empresa</h3>
-                        <textarea class="input-texto" placeholder="Edite o texto aqui..."></textarea>
-                    </div>
-
-                    <div class="area-cat-num">
-                        <div class="cat">
-                            <h3>Categoria</h3>
-                            <p>Artesanato</p>
+                <form action="#" method="POST" enctype="multipart/form-data">
+                    <div class="linha"></div>
+                    <!-- Lado esquerdo - Upload de imagem -->
+                    <div class="lado-esquerdo">
+                        <div class="area-logo">
+                            <label for="imagem" class="uploads">
+                                <input type="file" name="imagem" id="imagem" class="img-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/logo-marca.png" alt="Imagem do perfil">
+                            </label>
                         </div>
-                        <div class="num">
-                            <h3>Numero</h3>
-                            <p>89</p>
-                        </div>
+                        <div class="text-produtos"><h1>Produtos</h1></div>
+                        
+                        <div class="area-imgs"></div>
                     </div>
 
-                    <div class="area-inf-corR">
-                        <div class="area-form">
-                            <h3>Informações Pessoais</h3>
-                                <form action="salvar_perfil.php" method="POST" enctype="multipart/form-data">
-                                    <div class="area-infs">
-                                        <!-- Lado esquerdo - Upload de imagem -->
-                                        <div class="lado-esquerdo">
-                                            <div class="area-logo">
-                                                <label for="imagem" class="uploads">
-                                                    <input type="file" name="imagem" id="imagem" class="img-input" multiple>
-                                                    <img src="../../../Public/imgs/imgs-edicao-perfil-expo/logo-marca.png" alt="Imagem do perfil">
-                                                </label>
-                                            </div>
-                                            <div class="text-produtos"></div>
-                                            <div class="area-imgs"></div>
-                                        </div>
+                    <!-- Lado direito - Informações -->
+                    <div class="lado-direito">
+                        <div class="area-text">
+                            <h1 class="title">Play Artesanato</h1>
+                            <p class="sobre">Sobre a Empresa</p>
+                            <textarea name="descricao" class="input-texto" placeholder="Edite o texto aqui..."></textarea>
+                        </div>
 
-                                        <!-- Lado direito - Informações -->
-                                        <div class="lado-direito">
-                                            <div class="area-text">
-                                                <h1 class="title">Play Artesanato</h1>
-                                                <h3 class="sobre">Sobre a Empresa</h3>
-                                                <textarea name="descricao" class="input-texto" placeholder="Edite o texto aqui..."></textarea>
-                                            </div>
-
-                                            <div class="area-cat-num">
-                                                <div class="cat">
-                                                    <h3>Categoria</h3>
-                                                    <p>Artesanato</p>
-                                                </div>
-                                                <div class="num">
-                                                    <h3>Numero</h3>
-                                                    <p>89</p>
-                                                </div>
-                                            </div>
-
-                                            <div class="area-inf-corR">
-                                                <div class="area-form">
-                                                    <h3>Informações Pessoais</h3>
-                                                    <div class="area-inputs">
-                                                        <label>Link Instagram:</label>
-                                                        <input class="input" type="text" name="insta" placeholder="Digite o Link do Instagram" required>
-                                                    </div>
-
-                                                    <div class="area-inputs">
-                                                        <label>Link Whatsapp:</label>
-                                                        <input class="input" type="text" name="whatsapp" placeholder="Digite o Link do WhatsApp" required>
-                                                    </div>
-
-                                                    <div class="area-inputs">
-                                                        <label>Link Facebook:</label>
-                                                        <input class="input" type="text" name="facebook" placeholder="Digite o Link do Facebook" required>
-                                                    </div>
-
-                                                    <div class="area-inputs">
-                                                        <label>Link E-mail:</label>
-                                                        <input class="input" type="email" name="email" placeholder="Digite o Link do E-mail" required>
-                                                    </div>
-                                                </div>
-
-                                                <div class="area-corR">
-                                                    <h3>Cor da Rua</h3>
-                                                    <div class="cor-rua"><p>Verde</p></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
+                        <div class="area-cat-num">
+                            <div class="cat">
+                                <h3>Categoria</h3>
+                                <p class="p-cat">Artesanato</p>
                             </div>
-                        <div class="area-corR">
-                            <h3>Cor da Rua</h3>
-                            <div class="cor-rua"><p>Verde</p></div>
+                            <div class="num">
+                                <h3>Numero</h3>
+                                <p>89</p>
+                            </div>
+                        </div>
+
+                        <div class="area-inf-corR">
+                            <div class="area-total-form">
+                                <h3>Informações Pessoais</h3>
+                                <div class="area-inputs">
+                                    <label>Link Instagram:</label>
+                                    <input class="input" type="text" name="insta" placeholder="Digite o Link do Instagram" required>
+                                </div>
+
+                                <div class="area-inputs">
+                                    <label>Link Whatsapp:</label>
+                                    <input class="input" type="text" name="whatsapp" placeholder="Digite o Link do WhatsApp" required>
+                                </div>
+
+                                <div class="area-inputs">
+                                    <label>Link Facebook:</label>
+                                    <input class="input" type="text" name="facebook" placeholder="Digite o Link do Facebook" required>
+                                </div>
+
+                                <div class="area-inputs">
+                                    <label>Link E-mail:</label>
+                                    <input class="input" type="email" name="email" placeholder="Digite o Link do E-mail" required>
+                                </div>
+                            </div>
+
+                            <div class="area-corR">
+                                <h3>Cor da Rua</h3>
+                                <div class="cor-rua"><p>Verde</p></div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
-
     </section>
     
     
