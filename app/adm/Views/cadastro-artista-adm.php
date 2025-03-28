68f3e7f4497fba7115ff69735ba1c702d8673e60
Maykelly Castro <cezariomaykelly@gmail.com>
2025-03-28 14:36:29 -0400

    update

--- app/adm/Views/cadastro-artista-adm.php
+++ app/adm/Views/cadastro-artista-adm.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bosque da paz</title>
     <link rel="stylesheet" href="../../../Public/css/css-adm/style-cadastrar-artistas.css">
+    <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
 
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
