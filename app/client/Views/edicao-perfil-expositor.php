db4d2c006d98261b26d4a04a2386be2443654f5c
Matheus Manja <matheusmanja7@gmail.com>
2025-03-28 17:00:42 -0400

    comit atualizações tela de edição perfil do expositor

--- app/client/Views/edicao-perfil-expositor.php
+++ app/client/Views/edicao-perfil-expositor.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../Public/css/css-home/edicao-perfil-expositor.css">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-edicao-perfil-expositor.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <title>Document</title>
 </head>
+
 <body>
     <?php include "../../../Public/assets/adm/menu-adm.html" ?>
 
@@ -16,31 +18,73 @@
         <div class="img3"><img src="../../../Public/imgs/imgs-edicao-perfil-expo/img3.png" alt=""></div>
 
         <div class="box-principal">
-            <div class="area-h1">
-                <h1>Edição Perfil Expositor</h1>
-            </div>
             <div class="area-infs">
+                <div class="linha"></div>
                 <form action="#" method="POST" enctype="multipart/form-data">
-                    <div class="linha"></div>
                     <!-- Lado esquerdo - Upload de imagem -->
                     <div class="lado-esquerdo">
                         <div class="area-logo">
                             <label for="imagem" class="uploads">
                                 <input type="file" name="imagem" id="imagem" class="img-input" multiple>
-                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/logo-marca.png" alt="Imagem do perfil">
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/logo-marca.png"
+                                    alt="Imagem do perfil">
                             </label>
+                            <p>Clique na logo para alterar</p>
                         </div>
-                        <div class="text-produtos"><h1>Produtos</h1></div>
-                        
-                        <div class="area-imgs"></div>
+                        <div class="text-produtos">
+                            <h1>Produtos</h1>
+                        </div>
+
+                        <div class="area-imgs">
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-1.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-2.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-3.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-4.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-5.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+
+                            <label for="imagem" class="imgs-produtos">
+                                <input type="file" name="imagem" id="imagem" class="img-produto-input" multiple>
+                                <img src="../../../Public/imgs/imgs-edicao-perfil-expo/foto-produto-6.jpeg"
+                                    alt="Imagem do perfil">
+                            </label>
+                            <p>Clique na imagem para alterar</p>
+                        </div>
                     </div>
 
                     <!-- Lado direito - Informações -->
                     <div class="lado-direito">
+                        <div class="area-h1">
+                            <h1>Edição Perfil Expositor</h1>
+                        </div>
                         <div class="area-text">
                             <h1 class="title">Play Artesanato</h1>
                             <p class="sobre">Sobre a Empresa</p>
-                            <textarea name="descricao" class="input-texto" placeholder="Edite o texto aqui..."></textarea>
+                            <textarea name="descricao" class="input-texto"
+                                placeholder="Edite o texto aqui..."></textarea>
                         </div>
 
                         <div class="area-cat-num">
@@ -52,44 +96,68 @@
                                 <h3>Numero</h3>
                                 <p>89</p>
                             </div>
+
+                            <div class="area-corR">
+                                <h3>Cor da Rua</h3>
+                                <div class="cor-rua">
+                                    <p>Verde</p>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="area-inf-corR">
                             <div class="area-total-form">
-                                <h3>Informações Pessoais</h3>
+                                <h3 class="h3-infs-pessoais">Informações Pessoais</h3>
                                 <div class="area-inputs">
                                     <label>Link Instagram:</label>
-                                    <input class="input" type="text" name="insta" placeholder="Digite o Link do Instagram" required>
+                                    <div class="input-container">
+                                        <i class="bi bi-instagram"></i>
+                                        <input class="input" type="text" name="insta"
+                                            placeholder="Digite o Link do Instagram" required>
+                                    </div>
                                 </div>
 
                                 <div class="area-inputs">
                                     <label>Link Whatsapp:</label>
-                                    <input class="input" type="text" name="whatsapp" placeholder="Digite o Link do WhatsApp" required>
+                                    <div class="input-container">
+                                        <i class="bi bi-whatsapp"></i>
+                                        <input class="input" type="text" name="whatsapp"
+                                            placeholder="Digite o Link do WhatsApp" required>
+                                    </div>
                                 </div>
 
                                 <div class="area-inputs">
                                     <label>Link Facebook:</label>
-                                    <input class="input" type="text" name="facebook" placeholder="Digite o Link do Facebook" required>
+                                    <div class="input-container">
+                                        <i class="bi bi-facebook"></i>
+                                        <input class="input" type="text" name="facebook"
+                                            placeholder="Digite o Link do Facebook" required>
+                                    </div>
                                 </div>
 
                                 <div class="area-inputs">
                                     <label>Link E-mail:</label>
-                                    <input class="input" type="email" name="email" placeholder="Digite o Link do E-mail" required>
+                                    <div class="input-container">
+                                        <i class="bi bi-envelope"></i>
+                                        <input class="input" type="email" name="email"
+                                            placeholder="Digite o Link do E-mail" required>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="area-corR">
-                                <h3>Cor da Rua</h3>
-                                <div class="cor-rua"><p>Verde</p></div>
-                            </div>
+                        <div class="area-buttons">
+                            <button class="buttons" id="button-cancelar">Cancelar</button>
+                            <button class="buttons" id="button-salvar">Salvar</button>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
     </section>
-    
-    
+
+
 
 </body>
+
 </html>
