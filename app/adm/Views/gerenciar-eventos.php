93cf23ea4a462e37954db5c6e145a9e0282fcd0a
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-04-01 17:15:21 -0400

    new menu adm

--- app/adm/Views/gerenciar-eventos.php
+++ app/adm/Views/gerenciar-eventos.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../Public/css/css-adm/style-gerenciar-eventos.css">
-    <link rel="stylesheet" href="../../../Public/css/menu-adm.css">
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
     <title>Adm - Bosque da Paz</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
