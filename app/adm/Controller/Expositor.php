02caaca61e3f6b070a50c2c79182fc5dd486bc51
Maykelly Castro <cezariomaykelly@gmail.com>
2025-04-03 17:21:41 -0400

    update

--- app/adm/Controller/Expositor.php
+++ app/adm/Controller/Expositor.php
@@ -1,5 +1,6 @@
 <?php
 require '../Models/Controller-ADM.php';
+require '../Controller/Imagem.php';
 
 
 class Expositor extends Pessoa
@@ -8,7 +9,7 @@ class Expositor extends Pessoa
     protected $id_expositor;
     protected $id_pessoa;
     protected $id_categoria;
-    protected $id_imagem;
+    // protected $id_imagem;
     protected $nome_marca;
     protected $num_barraca;
     protected $voltagem;
@@ -19,11 +20,17 @@ class Expositor extends Pessoa
     protected $cor_rua;
     protected $responsavel;
     protected $produto;
+    protected $imagens;
 
 
     public function setId_expositor($id_expositor)
     {
         $this->id_expositor = $id_expositor;
+    }
+
+    public function setImagens($imagens)
+    {
+        $this->imagens = $imagens;
     }
 
     public function setd_pessoa($id_pessoa)
@@ -34,15 +41,15 @@ class Expositor extends Pessoa
     {
         $this->id_categoria = $id_categoria;
     }
-    public function setId_imagem($id_imagem)
-    {
-        $this->id_imagem = $id_imagem;
-    }
+    // public function setId_imagem($id_imagem)
+    // {
+    //     $this->id_imagem = $id_imagem;
+    // }
     public function setNome_marca($nome_marca)
     {
         $this->nome_marca = $nome_marca;
     }
- 
+
     public function setEnergia($energia)
     {
         $this->energia = $energia;
@@ -93,10 +100,10 @@ class Expositor extends Pessoa
     {
         return $this->id_categoria;
     }
-    public function getId_imagem()
-    {
-        return $this->id_imagem;
-    }
+    // public function getId_imagem()
+    // {
+    //     return $this->id_imagem;
+    // }
     public function getNome_marca()
     {
         return $this->nome_marca;
@@ -151,12 +158,56 @@ class Expositor extends Pessoa
             ]
         );
 
+        
+        //     // Verifica se existem arquivos e os percorre
+        // for ($i = 0; $i < count($this->imagens); $i++) {
+
+        //     // Verifica se houve erro no upload
+        //     // if ($fileError === UPLOAD_ERR_OK) {
+        //     //     $uploadDir = 'uploads/'; // Define o diretório de upload
+        //     //     $uploadFile = $uploadDir . basename($fileName);
+
+        //     //     // Move o arquivo para o diretório final
+        //     //     if (move_uploaded_file($fileTmp, $uploadFile)) {
+
+        //     //         $db = new Database('imagem');
+        //     //         $img_id = $db->insert_lastid([
+        //     //             'imagem1' => "$fileName",
+        //     //             'imagem2' => "$fileName",
+        //     //             'imagem3' => "$fileName",
+        //     //             'imagem4' => "$fileName",
+        //     //             'imagem5' => "$fileName"
+        //     //         ]);
+        //     //         $fileIds[] = $img_id;
+        //     //     }
+        //     // }
+
+        //     $db = new Database('imagem');
+        //     $img_id = $db->insert_lastid([
+        //         'imagem1' => $this->imagens[0],
+        //         'imagem2' => $this->imagens[1],
+        //         'imagem3' => $this->imagens[2],
+        //         'imagem4' => $this->imagens[3],
+        //         'imagem5' => $this->imagens[4]
+        //     ]);
+        // }
+
+        $db = new Database('imagem');
+        $img_id = $db->insert_lastid([
+            'imagem1' => $this->imagens[0],
+            'imagem2' => $this->imagens[1],
+            'imagem3' => $this->imagens[2],
+            'imagem4' => $this->imagens[3] ?? '',
+            'imagem5' => $this->imagens[4] ?? ''
+        ]);
+
+
         $db = new Database('expositor');
         $res = $db->insert(
             [
                 'id_expositor' => $this->id_expositor,
                 'id_pessoa' => $pes_id,
-                'id_categoria' => 1, // $this->id_categoria,
+                'id_categoria' => $this->id_categoria,
                 'id_imagem' => 1, // $img_id,
                 'nome_marca' => $this->nome_marca,
                 'num_barraca' => $this->num_barraca,
