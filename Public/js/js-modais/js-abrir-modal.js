197e5556e4a76d1b050f540d017ffef0da56c530
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-04-08 14:54:00 -0400

    modal cadastrar categoria

--- Public/js/js-modais/js-abrir-modal.js
+++ Public/js/js-modais/js-abrir-modal.js
@@ -1,9 +1,12 @@
 const openButtons = document.querySelectorAll('.open-modal');
+console.log(openButtons)
 
 openButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modalId = button.getAttribute('data-modal');
+        console.log(modalId)
         const modal = document.getElementById(modalId);
+        console.log(modal)
         modal.showModal();
     })
 })
