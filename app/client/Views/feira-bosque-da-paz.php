ba4a973ac915fbcf542f9ee1da0fe14d983f09ea
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-04-10 16:41:17 -0400

    refazendo home

--- app/client/Views/feira-bosque-da-paz.php
+++ app/client/Views/feira-bosque-da-paz.php
@@ -4,43 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bosque da Paz</title>
-    <link rel="stylesheet" href="../../../Public/css/css-home/style-feira-bosque-da-paz.css">
+    <!-- <link rel="stylesheet" href="../../../Public/css/css-home/style-feira-bosque-da-paz.css">
     <link rel="stylesheet" href="../../../Public/css/css-modais/perfil-expositor.css">
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico" >
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../../../Public/css/css-home/css-parceiros-home.css">
+    <link rel="stylesheet" href="../../../Public/css/css-home/css-parceiros-home.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-menu.css">
 </head>
 <!-- inicio body -->
-<body class="home">
-    <!-- incluindo o menu -->
-    <?php include "../../../Public/assets/home/menu-home.html"; ?>
-    <?php include "../../../Public/assets/home/carrossel-home.php"; ?>
-        
-    <!-- importando informações da feira -->
-    <?php include "../../../Public/assets/home/info-sobre-feira.html"; ?>
-    
-    <!-- incluindo expositores -->
-    <?php include "../../../Public/assets/home/lista-expositor.php"; ?>
-    
-    <!-- importando categoria -->
-    <?php include "../../../Public/assets/home/categoria.html"; ?>
+<body class="home" id="corpo">
+    <header class="cabecalho">
+        <nav class="list-menu">
+            <div class="content content-logo">
+                <a href="#" class="link-home">
+                    <img src="../../../Public/imgs/img-home/logo.png" alt="logo" class="img-logo">
+                </a>
+            </div>
 
-<!-- fim main -->
+            <ul class="content nav-list" rule="link pages">
+                <li class="nav-item">ínicio</li>
+                <li class="nav-item">exepositores</li>
+                <li class="nav-item">fale conosco</li>
+                <li class="nav-item">quem somos</li>
+            </ul>
 
-    <!-- importando avisos -->
-    <?php include "../../../Public/assets/home/avisos.html"; ?>
-
-    <!-- importando mapa -->
-    <?php include "../../../Public/assets/home/mapa.html"; ?>
-    <?php include "../../../Public/assets/home/parceiros.html"; ?>
-
-    <!-- importando o rodape -->
-    <?php include "../../../Public/assets/home/rodape.html"; ?>
-
-    <script src="../../../Public/js/js-home/main.js" defer></script>
-    <script src="../../../Public/js/js-home/carrossel.js" defer></script>
-    <script src="../../../Public/js/js-modais/js-abrir-modal.js" defer></script>
-    <script src="../../../Public/js/js-home/subir-num.js"></script>
+            <div class="content conteiner-outros">
+                <div class="content-sandwich">
+                    <!-- <img src="../../../Public/imgs/img-home/" alt="" class="sandwich"> -->
+                    <i class="bi bi-list sandwich" id="sandwich"></i>
+                </div>
+                <div class="content-login">
+                    <a href="" target="_blank" class="link-login">
+                        <!-- <img src="../../../Public/imgs/img-home/login.png" alt="" class="login"> -->
+                        <i class="bi bi-person-circle login"></i>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
 </body>
 </html>
+
+<!-- <?php //include "../../../Public/assets/home/menu-home.html"; ?> -->
+<!-- <?php //include "../../../Public/assets/home/carrossel-home.php"; ?> -->
+    
+<!-- <?php //include "../../../Public/assets/home/info-sobre-feira.html"; ?> -->
+
+<!-- <?php //include "../../../Public/assets/home/lista-expositor.php"; ?> -->
+
+<!-- <?php //include "../../../Public/assets/home/categoria.html"; ?> -->
+
+<!-- <?php //include "../../../Public/assets/home/avisos.html"; ?> -->
+
+<!-- <?php //include "../../../Public/assets/home/mapa.html"; ?> -->
+<!-- <?php //include "../../../Public/assets/home/parceiros.html"; ?> -->
+
+<!-- <?php //include "../../../Public/assets/home/rodape.html"; ?>  -->
+
+<!-- <script src="../../../Public/js/js-home/main.js" defer></script>
+<script src="../../../Public/js/js-home/carrossel.js" defer></script>
+<script src="../../../Public/js/js-modais/js-abrir-modal.js" defer></script>
+<script src="../../../Public/js/js-home/subir-num.js"></script> -->
