cc26afdc83fa64c295e491697731e9ba65b66257
halysonreis <halysondasilvadosreis@gmail.com>
2025-04-12 21:07:16 -0400

    fiz carrossel sem tutorial, me sinto foda

--- Public/js/js-home/main.js
+++ Public/js/js-home/main.js
@@ -21,22 +21,21 @@ function menuShowHome() {
     }
 }
 
+// pegando elementos do carrossel
 const arrowLeft = document.getElementById('arrow-left');
 const arrowRight = document.getElementById('arrow-right');
 const sliders = document.querySelectorAll('#slider-content')
 const balls = document.querySelectorAll('#ball')
-console.log(arrowLeft);
-console.log(arrowRight);
-console.log(balls);
+//iniciando valores padrao
 let count_carrossel = 0;
 let slider = sliders[0];
 let bola_carrossel = balls[0]
 const color_carrossel = '#0D592E';
-console.log(slider);
 
 slider.style.zIndex = 1;
 bola_carrossel.style.backgroundColor = color_carrossel;
 
+// function de trocar imagem
 function trocaImagem (n){
     slider.style.zIndex = 0;
     bola_carrossel.style.backgroundColor = 'white';
@@ -47,6 +46,7 @@ function trocaImagem (n){
     count_carrossel = n
 }
 
+// intervalo de troca de imagem
 const interval = setInterval(() => {
     if (count_carrossel == 0){
         trocaImagem(1)
@@ -62,7 +62,7 @@ const interval = setInterval(() => {
     }
 }, 3000)
 
-
+// ao clicar na seta esquerca volta para a imagem anterior
 arrowLeft.addEventListener('click', () => {
     if (count_carrossel == 0){
         trocaImagem(2)
@@ -75,6 +75,7 @@ arrowLeft.addEventListener('click', () => {
     }
 })
 
+// ao clicar na seta direita passa de imagem
 arrowRight.addEventListener('click', () => {
     if (count_carrossel == 0){
         trocaImagem(1)
