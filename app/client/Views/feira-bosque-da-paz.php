d7527105ee95852b96fb910b7efd2aab431806c6
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-04-16 14:19:51 -0400

    start categorias

--- app/client/Views/feira-bosque-da-paz.php
+++ app/client/Views/feira-bosque-da-paz.php
@@ -21,6 +21,99 @@
     <?php include "../../../Public/assets/home/info-sobre-feira.html"; ?>
     
     <?php include "../../../Public/assets/home/lista-expositor.php"; ?>
+
+    <section class="categoria">
+        <div class="bar-title-categoria">
+            <div class="conteiner-title-cat">
+                <div class="line-before"></div>
+                <h2 class="all-titles title-categoria">categoria de expositores</h2>
+                <div class="line-after"></div>
+            </div>
+            <p class="para-title-cat all-para">Veja como são nossos expositores e venha explorar, apoiar e compartilhar uma experiência inesquecível conosco!</p>
+        </div>
+        <div class="categorias" id="categorias">
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo artesanato">
+                        <img src="../../../Public/imgs/img-home/picel.png" alt="Imagem picel" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">ARTESANATO</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#antiguidade" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo antiguidade">
+                        <img src="../../../Public/imgs/img-home/Parchment.png" alt="Imagem papel" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">ANTIGUIDADE</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#colecionismo" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo colecionismo">
+                        <img src="../../../Public/imgs/img-home/NFT Collection.png" alt="Imagem coleção" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">COLECIONISMO</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo cosmetica">
+                        <img src="../../../Public/imgs/img-home/Vial.png" alt="Imagem ampola" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">COSMETOLOGIA</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo gastronomia">
+                        <img src="../../../Public/imgs/img-home/Real Food for Meals.png" alt="Imagem comida" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">GASTRONOMIA</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo literatura">
+                        <img src="../../../Public/imgs/img-home/Books.png" alt="Imagem livros" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">LITERATURA</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo moda">
+                        <img src="../../../Public/imgs/img-home/Scissors.png" alt="Imagem tesoura" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">MODA AUTORAL</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo planta">
+                        <img src="../../../Public/imgs/img-home/Potted Plant.png" alt="Imagem planta" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">PLANTAS</p>
+                </div>
+            </a>
+
+            <a href="todos-expositores.php#artesanato" class="link-cat-expo">
+                <div class="item">
+                    <div class="img-fundo sustenta">
+                        <img src="../../../Public/imgs/img-home/Recycle.png" alt="Imagem reciclagem" class="img-categoria">
+                    </div>
+                    <p class="name-categoria">SUSTENTABILIDADE</p>
+                </div>
+            </a>
+        </div>
+    </section>
     
         
     </main>
