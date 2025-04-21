97bd4f56e950a0e5917a1ee88fbca2c8a2dd6a12
halysonreis <halysondasilvadosreis@gmail.com>
2025-04-20 21:30:07 -0400

    proximo evento iniciado

--- app/client/Views/feira-bosque-da-paz.php
+++ app/client/Views/feira-bosque-da-paz.php
@@ -24,8 +24,49 @@
 
     <?php include "../../../Public/assets/home/categoria.html"; ?>
 
-    <section class="" id="">
-
+    <section class="eventos" id="eventos">
+        <div class="prox-evento">
+            <div class="contenier-prox-evento">
+                <div class="content-title-evento">
+                    <h2 class="title-prox-evento all-titles" id="title-prox-evento">proximo evento</h1>
+                    <p class="all-para para-prox-evento" id="para-prox-evento">descubra como sera nosso próximo evento e atrações</p>
+                </div>
+                <div class="content-prox-evento">
+                    <!-- evento -->
+                    <div class="content-img-evento">
+                        <img class="img-banner-event" id="img-banner-evento" src="../../../Public/imgs/Proximos-Eventos-img/img-evento-proev.png">
+                        <div class="content-info-prox-event">
+                            <h3 class="all-titles title-banner-prox" id="title-prox-event">dia 10/05</h3>
+                            <p class="all-para para-banner-prox" id="para-banner-prox">das 8:00 as 16:00</p>
+                            <span class="descricao-banner-evento" id="descricao-banner-evento">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </span>
+                        </div>
+                    </div>
+                    <!-- atracoes -->
+                    <div class="content-atracoes-evento">
+                        <h3 class="all-titles title-atracoes-evento" id="title-atracoes-evento">atrações</h3>
+                        <div class="content-card-atracoes">
+                            <div class="card-eventos card-atracoes-event" id="card-atracoes-event">
+                                <img class="img-card-evento img-card-atracoes" id="img-card-atracoes" src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png">
+                                <div class="content-info-card-event">
+                                    <h4 class="all-title title-info-card title-info-card-atracoes" id="title-info-card-atracoes">title atracao</h4>
+                                    <button class="btn-info-card btn-card-atracoes" id="btn-card-atracoes">saiba mais</button>
+                                </div>
+                            </div>
+                            <div class="card-eventos card-atracoes-event" id="card-atracoes-event">
+                                <img class="img-card-evento img-card-atracoes" id="img-card-atracoes" src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png">
+                                <div class="content-info-card-event">
+                                    <h4 class="all-title title-info-card title-info-card-atracoes" id="title-info-card-atracoes">title atracao</h4>
+                                    <button class="btn-info-card btn-card-atracoes" id="btn-card-atracoes">saiba mais</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+               
+            </div>
+        </div>
     </section>
     </main>
 
