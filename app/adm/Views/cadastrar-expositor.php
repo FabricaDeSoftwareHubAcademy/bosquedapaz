4850068bc2423fde6352193428c7e5b0b17092c4
Maykelly Castro <cezariomaykelly@gmail.com>
2025-05-12 15:27:32 -0400

    Update cadastrar-expositor.php

--- app/adm/Views/cadastrar-expositor.php
+++ app/adm/Views/cadastrar-expositor.php
@@ -25,7 +25,7 @@ if (isset($_POST['REQUEST_METHOD'])) {
 
     $res = $expositor->cadastrar();
 
-    
+
     exit;
 }
 
@@ -65,8 +65,11 @@ if (isset($_POST['REQUEST_METHOD'])) {
                         </div>
                         <div class="input">
                             <label>Whatsapp:</label>
-                            <input type="text" name="whatsapp" id="" placeholder="Número de whatsapp" required>
+                            <input type="tel" name="whatsapp" placeholder="Número de whatsapp"
+                                pattern="[0-9]{10,11}" required
+                                oninput="this.value = this.value.replace(/[^0-9]/g, '')">
                         </div>
+
 
                         <div class="input">
                             <label>E-mail:</label>
