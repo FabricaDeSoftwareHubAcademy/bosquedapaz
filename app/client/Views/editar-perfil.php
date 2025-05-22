ecaea41266dd1557c17f3904115d6af76f37a851
TiagoSnts <tsc.3602006@gmail.com>
2025-05-22 17:31:52 -0400

    atualizacoes

--- app/client/Views/editar-perfil.php
+++ app/client/Views/editar-perfil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Perfil</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../../../Public/css/style-editar-perfil.css">
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
@@ -16,6 +16,7 @@
         <h1 class="perfilEdit-title">Editar Perfil</h1>
 
         <form action="#" class="perfilEdit-form">
+
             <section class="perfilEdit-empresas">
                 <div class="perfilEdit-logo">
                     <label for="foto" class="perfilEdit-logoEmpresa">Logo da empresa: </label>
@@ -26,41 +27,60 @@
                     <input type="text" name="nome" id="nome" class="perfilEdit-nome" placeholder="Digite seu nome" required>
                 </div>
             </section>
+
             <section class="perfilEdit-desc">
                 <label class="perfilEdit-label-desc" for="descricao" >Sobre a Empresa: </label>
                 <textarea name="descricao" id="descricao" cols="200" rows="10" class="perfilEdit-text-desc" placeholder="Digite uma breve descrição sobre sua empresa"></textarea>
             </section>
+            
             <section class="perfilEdit-info">
                 <div>
-                    <img src="https://img.icons8.com/ios-filled/32/228BE6/instagram-new--v1.png" alt="instagram-new--v1"/>
-                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info">
+                    <i class="fa-brands fa-square-instagram"></i>
+                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info" placeholder="Digite seu instagram">
                 </div>
                 <div>
-                    <img src="https://img.icons8.com/ios-filled/50/22bc9d/whatsapp--v1.png" alt="whatsapp--v1"/>
-                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info">
+                    <i class="fa-brands fa-square-whatsapp"></i>
+                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info" placeholder="Digite seu whatsapp">
                 </div>
                 <div>
-                    <img src="https://img.icons8.com/ios-glyphs/30/FAB005/facebook-new.png" alt="facebook-new"/>
-                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info">
+                    <i class="fa-brands fa-square-facebook"></i>
+                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info" placeholder="Digite seu facebook">
                 </div>
                 <div>
-                    <img src="https://img.icons8.com/windows/32/ff3877/secured-letter--v2.png" alt="secured-letter--v2"/>
-                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info">
+                    <i class="fa-solid fa-envelope"></i>
+                    <input type="text" name="informacao" id="informacao" class="perfilEdit-input-info" placeholder="Digite seu e-mail">
                 </div>
             </section>
+
             <section class="perfilEdit-foto-info">
-                <label for="" class="perfilEdit-foto-info-label"></label>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
-                <div class="perfilEdit-load-foto"> <input type="file" name="foto" id="foto" class="perfilEdit-input-foto"> </div>
+                <label for="" class="perfilEdit-foto-info-label">Selecione fotos de seus produtos:</label>
+                <div class="perfilEdit-div-group">
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                    <div class="perfilEdit-load-foto">
+                        <input type="file" name="foto" id="foto" class="perfilEdit-input-foto">
+                    </div>
+                </div>
             </section>
+
             <section class="perfilEdit-btn">
                 <button>Cancelar</button>
                 <button>Salvar</button>
             </section>
+
         </form>
         <img src="../../../Public/imgs/img-login/arrow-circle-left.svg" alt=""><a href=""></a></img>
      </div>
