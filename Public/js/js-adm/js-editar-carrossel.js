9f4e021cc24d7d0aa531362d6b899ac9c8969035
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-05-23 17:26:23 -0400

    cadastro foi

--- Public/js/js-adm/js-editar-carrossel.js
+++ Public/js/js-adm/js-editar-carrossel.js
@@ -2,9 +2,9 @@
 function readImage() {
     if (this.files && this.files[0]) {
         var file = new FileReader();
-        file.onload = function(e) {
+        file.onload = function (e) {
             document.getElementById("img1").src = e.target.result;
-        };       
+        };
         file.readAsDataURL(this.files[0]);
     }
 }
@@ -12,9 +12,9 @@ function readImage() {
 function readImage2() {
     if (this.files && this.files[0]) {
         var file = new FileReader();
-        file.onload = function(e) {
+        file.onload = function (e) {
             document.getElementById("img2").src = e.target.result;
-        };       
+        };
         file.readAsDataURL(this.files[0]);
     }
 }
@@ -22,9 +22,9 @@ function readImage2() {
 function readImage3() {
     if (this.files && this.files[0]) {
         var file = new FileReader();
-        file.onload = function(e) {
+        file.onload = function (e) {
             document.getElementById("img3").src = e.target.result;
-        };       
+        };
         file.readAsDataURL(this.files[0]);
     }
 }
@@ -40,85 +40,36 @@ let inputImg3 = document.getElementById("imagens-input3")
 inputImg3.addEventListener("change", readImage3, false);
 
 
-$btnEditar = document.getElementById('editar');
+btnEditar = document.getElementById('editar');
 // console.log($formCarrossel);
 
-$btnEditar.addEventListener('click', async function (event){
+btnEditar.addEventListener('click', async function (event) {
     event.preventDefault()
 
-    let formCarrossel = document.querySelectorAll('[type=file]');
-    
-    const formData = new FormData(formCarrossel.target);
+    let formCarrossel = document.getElementById('form-carrossel')
 
-    // var img1 = new Object()
-    // var img2 = new Object()
-    // var img3 = new Object()
+    const formData = new FormData(formCarrossel);
 
-    if(formCarrossel[0].files.length == 1){
-        var dados_img1 = formCarrossel[0].files;
-        
-        for (const chave in dados_img1) {
-            img1[chave] = dados_img1[chave]
-          }
-        img1["possicao"] = 1
-        // console.log(dados_img1)
-    }
-    
-    // if(formCarrossel[1].files.length == 1){
-    //     var dados_img2 = formCarrossel[0].files[0];
-        
-    //     for (const chave in dados_img2) {
-    //         img2[chave] = dados_img2[chave]
-    //     }
-    //     img2["possicao"] = 2
-    //     formData.append("files[]", img2)
-    // }
-    
-    // if(formCarrossel[2].files.length == 1){
-    //     var dados_img3 = formCarrossel[0].files[0];
-        
-    //     for (const chave in dados_img3) {
-    //         img3[chave] = dados_img3[chave]
-    //     }
-    //     img3["possicao"] = 3
-    //     formData.append("files[]", img3)
-    // }
+    // console.log(formData)
 
-
-
-    for(var i = 0; i < formCarrossel.length; i++){
-        var file = formCarrossel[i].files
-        for(var y = 0; y < file.length; y++){
-            // img1.append("files[]", file[y])
-            formData.append("files[]", file[y])
-        }
-        formData.append('num', i)
-    }
-
-
-    let dados_php = await fetch("../../../actions/carrossel.php",{
-        method:"POST",
-        body:formData
+    let dados_php = await fetch("../../../actions/carrossel.php", {
+        method: "POST",
+        body: formData
     });
 
     let response = await dados_php.json();
 
-    // console.log(img1)
     console.log(response)
 })
 
-async function getImage(){
-    let imagens = await fetch("../../../actions/carrossel.php?id=1")
+async function getImage() {
+    let imagens = await fetch("../../../actions/carrossel.php")
 
     let resposta = await imagens.json()
 
     var img1 = document.getElementById('img1');
     var img2 = document.getElementById('img2');
     var img3 = document.getElementById('img3');
-
-    img1.src = `../../${resposta[0]['img1']}`;
-    img2.src = `../../${resposta[0]['img2']}`;
-    img3.src = `../../${resposta[0]['img3']}`;
 }
 
 getImage()
--- actions/carrossel.php
+++ actions/carrossel.php
@@ -6,62 +6,42 @@ $car = new Carrossel();
 
 function update_carrossel($img,$num) {
     $caminho = '../Public/uploads/uploads-carrosel/';
-    $new_img = $img['name'][$num];
-    $new_name = 'img-carrossel-'.$num+1;
+    $new_img = $img['name'];
+    $new_name = 'img-carrossel-'.$num;
     $extencao_imagem = strtolower(pathinfo($new_img, PATHINFO_EXTENSION));
 
     $caminho_img = $caminho . $new_name. '.'. $extencao_imagem;
         
-    $upload_img = move_uploaded_file($img['tmp_name'][$num], $caminho_img);
+    $upload_img = move_uploaded_file($img['tmp_name'], $caminho_img);
 
     return $caminho_img;
 }
 
-
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $busca = $car->buscar_id(1);
-    $response = array("status" => $_FILES);
+    $response = array('status' => 200);
+
+    $i = 1;
+
+    foreach ($_FILES as $chave => $dados) {
+        if(!empty($dados['name'])){
+            $caminho = update_carrossel($dados, $i);
+            $car->caminho = $caminho;
+            $car->possicao = $i;
+            $atualizacao = $car->atualizar($i);
+            array_push($response, $atualizacao);
+        }
+
+        $i++;
+    }
+
     echo json_encode($response);
-    // $files = $_FILES['files'];
-    // if (!empty($files['name'][0])){
-    //     $img1 = update_carrossel($files, 0);
-    //     $car->img1 = $img1;
-    // }
-    // else {
-    //     $car->img1 = $busca->img1;
-    // }
-    // if (!empty($files['name'][1])){
-    //     $img2 = update_carrossel($files, 1);
-    //     $car->img2 = $img2;
-    // }
-    // else {
-    //     $car->img2 = $busca->img2;
-    // }
-    // if (!empty($files['name'][2])){
-    //     $img3 = update_carrossel($files, 2);
-    //     $car->img3 = $img3;
-    // }
-    // else {
-    //     $car->img3 = $busca->img3;
-    // }
-    
-    // try {
-    //     $res = $car->atualizar();
-    //     $status = $res ? 200 : 400;
-        
-    //     $response = array("status" => $status);
-    //     echo json_encode($response);
-    // } catch (\Throwable $th) {
-    //     $response = array("status" => 500);
-    //     echo json_encode($response);
-    // }
     
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $carrossel = $car->buscar_id(1);
+    $img = $car->get_imagem();
 
-    $response = array($carrossel, "status" => 200);
+    $response = array("imagens" => $img, "status" => 200);
     echo json_encode($response);
 }
 
