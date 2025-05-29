c147eec894a6b2e4ce0a0f5bbf9bb51a6150d14a
Teodororibas <teodororibas@icloud.com>
2025-05-29 16:14:56 -0400

    .

---
+++ Public/js/js-adm/cadastrar-parceiro.js
@@ -0,0 +1,25 @@
+let btn_cadastrar =  document.getElementById("botao_cadastrar");
+
+btn_cadastrar.addEventListener('click', async function(event){
+
+    event.preventDefault();
+    let formulario = document.getElementById("form_cadastrar_parceiro");
+
+    let formData = new FormData(formulario);
+
+    let dados_php = await fetch('../../../actions/cadastrar_parceiro.php',{
+        method:'POST',
+        body: formData
+    });
+    
+    let response = await dados_php.json();
+
+    if(response.status == 200){
+        console.log(response.msg);
+        alert("Cadastrado com sucesso!!!!") /// SUBSTITUIR PELO MODAL mostraModal()
+    }else{
+        console.log(response.msg);
+        alert("Cadastrado com sucesso!!!!") /// SUBSTITUIR PELO MODAL mostraModal()
+    }
+})
+
--- app/adm/Views/cadastrar-parceiros.php
+++ app/adm/Views/cadastrar-parceiros.php
@@ -117,37 +117,8 @@
     <script src="../../../Public/js/js-menu/js-menu.js" defer></script>
     <script src="../../../Public/js/js-adm/preview-img.js" defer></script>
     <script src="../../../Public/js/js-modais/js-abrir-modal.js" defer></script>
-
-
-    <script>
-
-        let btn_cadastrar =  document.getElementById("botao_cadastrar");
-
-        btn_cadastrar.addEventListener('click', async function(event){
-
-            event.preventDefault();
-            let formulario = document.getElementById("form_cadastrar_parceiro");
-
-            let formData = new FormData(formulario);
-
-            let dados_php = await fetch('../../../actions/cadastrar_parceiro.php',{
-                method:'POST',
-                body: formData
-            });
-            
-            let response = await dados_php.json();
-
-            if(response.status == 200){
-                console.log(response.msg);
-                alert("Cadastrado com sucesso!!!!") /// SUBSTITUIR PELO MODAL mostraModal()
-            }else{
-                console.log(response.msg);
-                alert("Cadastrado com sucesso!!!!") /// SUBSTITUIR PELO MODAL mostraModal()
-            }
-        })
- 
-    </script>
-
+    <script src="../../../Public/js/js-adm/cadastrar-parceiro.js" defer></script>
+    
 </body>
 
 </html>
