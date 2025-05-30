29f23f9e749585d99039335e039f8b7e60e318bc
Yan <yantmartins@live.com>
2025-05-30 16:22:02 -0400

    up css

--- app/Views/Client/apoiadores.php
+++ app/Views/Client/apoiadores.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico" >
 
     <link rel="stylesheet" href="../../../Public/css/css-home/style-menu.css">
-    <link rel="stylesheet" href="../../../Public/css/css-home/style-parceiros-home.css">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-apoiadores.css">
     <link rel="stylesheet" href="../../../Public/css/css-home/style-rodape.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
