19f6636d730f15c29a28a588bc68946ccf05924d
gcamargogy <camargogdy@hotmail.com>
2025-05-30 01:38:01 -0400

    boleto.php - 1

--- app/Controller/Boleto.php
+++ app/Controller/Boleto.php
@@ -1,36 +1,187 @@
 <?php
+
+namespace app\Controller;
+
+require_once('../vendor/autoload.php');
+
+use PDO;
+use app\Models\Database;
+
 class Boleto
 {
-    private $pdo;
+    private int $id_boleto;
+    private string $nome_expositor;
+    private string $cpf;
+    private string $boleto_pdf;
+    private string $vencimento;
+    private string $mes_referencia;
+    private float $valor;
+    private ?string $status_exp;
+    private int $id_expositor;
 
-    public function __construct(PDO $pdo)
+    // métodos getters
+    // ---------------------
+    public function getIdBoleto()
     {
-        $this->pdo = $pdo;
+        return $this->id_boleto;
     }
 
-    public function listarBoletos()
+    public function getNomeExpositor()
     {
-        $sql = "SELECT * FROM expositores";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->nome_expositor;
     }
 
-    public function atualizarSituacao($id, $novaSituacao)
+    public function getCPF()
     {
-        $sql = "UPDATE expositores SET situacao = :situacao WHERE id_expositor = :id";
-        $stmt = $this->pdo->prepare($sql);
-        return $stmt->execute([
-            ':situacao' => $novaSituacao,
-            ':id' => $id
+        return $this->cpf;
+    }
+
+    public function getBoletoPDF()
+    {
+        return $this->boleto_pdf;
+    }
+
+    public function getVencimento()
+    {
+        return $this->vencimento;
+    }
+
+    public function getMesReferencia()
+    {
+        return $this->mes_referencia;
+    }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    public function getStatusExp()
+    {
+        return $this->status_exp;
+    }
+
+    public function getIdExpositor()
+    {
+        return $this->id_expositor;
+    }
+    // ---------------------
+    // métodos getters
+
+    // métodos setters
+    // ---------------------
+    public function setIdBoleto($id_boleto)
+    {
+        $this->id_boleto = $id_boleto;
+        return $this;
+    }
+
+    public function setNomeExpositor($nome_expositor)
+    {
+        $this->nome_expositor = $nome_expositor;
+        return $this;
+    }
+
+    public function setCPF($cpf)
+    {
+        $this->cpf = $cpf;
+        return $this;
+    }
+
+    public function setBoletoPDF($boleto_pdf)
+    {
+        $this->boleto_pdf = $boleto_pdf;
+        return $this;
+    }
+
+    public function setVencimento($vencimento)
+    {
+        $this->vencimento = $vencimento;
+        return $this;
+    }
+
+    public function setMesReferencia($mes_referencia)
+    {
+        $this->mes_referencia = $mes_referencia;
+        return $this;
+    }
+
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+        return $this;
+    }
+
+    public function setStatusExp($status_exp)
+    {
+        $this->status_exp = $status_exp;
+        return $this;
+    }
+
+    public function setIdExpositor($id_expositor)
+    {
+        $this->id_expositor = $id_expositor;
+        return $this;
+    }
+    // ---------------------
+    // métodos setters
+
+    // funcionalidades
+    // ---------------------
+    public function CadastrarBoleto() {
+        $banco = new Database('boleto');
+        $execucao = $banco->insert([
+            'boleto_pdf' => $this->boleto_pdf,
+            'mes_referencia' => $this->mes_referencia,
+            'valor' => $this->valor,
+            'vencimento' => $this->vencimento
         ]);
+        return $execucao;
     }
 
-    public function buscarPorNome($nome)
-    {
-        $sql = "SELECT * FROM expositores WHERE nome_expositor LIKE :nome";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':nome' => "%$nome%"]);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function ListarBoletos(?string $nome = null) {
+        $bancoPessoa = new Database('pessoa');
+        $bancoExpositor = new Database('expositor');
+        $bancoBoleto = new Database('boleto');
+
+        $listagem = [];
+
+        if ($nome) { 
+            // verificando se existe uma pesquisa sendo realizada
+            // por um nome de expositor especifico. 
+            $pessoas = $bancoPessoa->select("nome LIKE '%{$nome}%'")->fetchAll(PDO::FETCH_ASSOC);
+            if (!$pessoas) {
+                return []; // retornando nenhuma pessoa encontrada. 
+            }
+        } else {
+            // se não existe nenhuma pesquisa sendo realizada
+            // a tabela volta a listar todas as pessoas.
+            $pessoas = $bancoPessoa->select()->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        foreach($pessoas as $pessoa) {
+            // verificando se a pessoa é um expositor.
+            $expositor = $bancoExpositor->select("id_pessoa = {$pessoa['id_pessoa']}")->fetch(PDO::FETCH_ASSOC);
+            if (!$expositor) continue; // se não é um expositor, é ignorado.
+
+            // verificando se o expositor possui um boleto.
+            $boleto = $bancoBoleto->select("id_expositor = {$expositor['id_expositor']}")->fetch(PDO::FETCH_ASSOC);
+            if (!$boleto) continue; // se não possui um boleto, não é listado.
+
+            // juntando todas as informações.
+            $dados = new Boleto();
+            $dados->id_boleto = $boleto['id_boleto'];
+            $dados->nome_expositor = $pessoa['nome'];
+            $dados->cpf = $pessoa['cpf'];
+            $dados->boleto_pdf = $boleto['pdf'];
+            $dados->vencimento = $boleto['vencimento'];
+            $dados->mes_referencia = $boleto['mes_referencia'];
+            $dados->valor = $boleto['valor'];
+            $dados->status_exp = $expositor['status_exp'];
+            $dados->id_expositor = $expositor['id_expositor'];
+
+            $listagem[] = $dados;
+        }
+        return $listagem;
     }
 }
