ec0bd1a834d2d1406c9f42c61d35ce065b427220
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-06-12 14:32:37 -0400

    btns

---
+++ Public/js/js-adm/js-btns-padrao.js
@@ -0,0 +1,24 @@
+let btnsPadrao = document.getElementById('btns-forms-padrao')
+btnsPadrao.style.width = '100%'
+btnsPadrao.style.padding = '2rem 2rem'
+btnsPadrao.style.display = 'flex'
+btnsPadrao.style.alignItems = 'center'
+btnsPadrao.style.justifyContent = 'space-between'
+btnsPadrao.style.backgroundColor = 'aqua'
+
+let btnVoltar = document.getElementById('btn-voltar')
+btnVoltar.style.backgroundColor = 'transparent'
+btnVoltar.style.width = '40px'
+btnVoltar.style.height = '40px'
+btnVoltar.style.borderRadius = '50%'
+btnVoltar.style.position = 'relative'
+btnVoltar.style.overflow = 'hidden'
+
+let linhaCima = document.getElementById('linha1')
+linhaCima.style.width = '1.1rem'
+linhaCima.style.height = '.1rem'
+linhaCima.style.borderRadius = '.2rem'
+linhaCima.style.margin = 'auto'
+// linhaCima.style.transform. = ''
+linhaCima.style.backgroundColor = 'black'
+
