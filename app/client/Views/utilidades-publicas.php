a8fd169850b54c917236077fb4076a66a7f4067a
Gabriel-A77 <galvesdesouza386@gmail.com>
2025-06-12 15:58:05 -0400

    comit2

--- app/client/Views/utilidades-publicas.php
+++ app/client/Views/utilidades-publicas.php
@@ -14,9 +14,7 @@
     <main class="principal-tela-utl">
         <div class="box-principal-utl">
             <h1 id="titulo-ult" class="title">Utilidades Públicas</h1>
-            
             <div class="area-cards">
-
                 <div class="cards" id="border-card-azul">
                     <div class="div__decorativa"></div>
                     <div class="linha__decorativa1"></div>
@@ -45,7 +43,6 @@
                             <button class="saiba-mais">Saiba Mais</button>
                         </div>
                     </div>
-                    
                 </div>
 
                 <div class="cards" id="border-card-azul">
@@ -61,7 +58,6 @@
                             <button class="saiba-mais">Saiba Mais</button>
                         </div>
                     </div>
-                    
                 </div>
 
                 <div class="cards" id="border-card-azul">
@@ -77,9 +73,7 @@
                             <button class="saiba-mais">Saiba Mais</button>
                         </div>
                     </div>
-                    
                 </div>
-
             </div>
             <div class="seta-volt">
                 <img src="../../../Public/imgs/img-editar-adm/btn-voltar.png" class="para-voltar" alt="">
@@ -87,85 +81,81 @@
         </div>
 
         <!-- MODAL 1 -->
+        <dialog class="dlog-modal-card-ProEv" id="modal-card-ProEv">
+            <div class="div-decorativa-modal">
+                <div id="linha-dec1-modal"></div>
+            </div>
+            <div class="div-decorativa2-modal">
+                <div id="linha-dec2-modal"></div>
+                <div id="linha-dec3-modal"></div>
+                <div id="linha-dec4-modal"></div>
+            </div>
+            <div class="div-infs-evento-modal">
+                <div class="areatl-img-modal-ProEv">
+                    <div class="area-img-modal-ProEv">
+                        <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
+                    </div>
+                </div>
+                <div class="area-text-modal-ProEv">
+                    <h1 class="title-atracao-modal">Tapioca Forró</h1>
+                    <p class="p-atracao-modal">Lorem ipsum dolor sit amet consectetur. 
+                        Fames metus ac egestas turpis ipsum Lorem 
+                        ipsum dolor sit amet consectetur. Lorem ipsum 
+                        dolor sit amet consectetur. Fames metus ac egestas 
+                        turpis ipsum Lorem ipsum dolor sit amet consectetur.
+                        Lorem ipsum 
+                        dolor sit amet consectetur. Fames metus ac egestas 
+                        turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <button id="b-sari-modal" class="close-modal" data-modal="modal-card-ProEv">
+                    <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
+                </button>
+            </div>
+        </dialog>
 
-                        <dialog class="dlog-modal-card-ProEv" id="modal-card-ProEv">
-                            <div class="div-decorativa-modal">
-                                <div id="linha-dec1-modal"></div>
-                            </div>
-                            <div class="div-decorativa2-modal">
-                                <div id="linha-dec2-modal"></div>
-                                <div id="linha-dec3-modal"></div>
-                                <div id="linha-dec4-modal"></div>
-                            </div>
-                            <div class="div-infs-evento-modal">
-                                <div class="areatl-img-modal-ProEv">
-                                    <div class="area-img-modal-ProEv">
-                                        <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="area-text-modal-ProEv">
-                                    <h1 class="title-atracao-modal">Tapioca Forró</h1>
-                                    <p class="p-atracao-modal">Lorem ipsum dolor sit amet consectetur. 
-                                        Fames metus ac egestas turpis ipsum Lorem 
-                                        ipsum dolor sit amet consectetur. Lorem ipsum 
-                                        dolor sit amet consectetur. Fames metus ac egestas 
-                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.
-                                        Lorem ipsum 
-                                        dolor sit amet consectetur. Fames metus ac egestas 
-                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <button id="b-sari-modal" class="close-modal" data-modal="modal-card-ProEv">
-                                    <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
-                                </button>
-                            </div>
-                        </dialog>
+        <!-- MODAL 2 -->
+            <dialog class="dlog-modal2-card-ProEv" id="modal2-card-ProEv">
+                <div class="div-decorativa-modal2">
+                    <div id="linha-dec1-modal2"></div>
+                </div>
+                <div class="div-decorativa2-modal2">
+                    <div id="linha-dec2-modal2"></div>
+                    <div id="linha-dec3-modal2"></div>
+                    <div id="linha-dec4-modal2"></div>  
+                </div>
+                <div class="div-infs-evento-modal2">
+                    <div class="areatl-img-modal2-ProEv">
+                        <div class="area-img-modal2-ProEv">
+                            <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
+                        </div>
+                    </div>
+                    <div class="area-text-modal2-ProEv">
+                        <h1 class="title-atracao-modal2">Sampari</h1>
+                        <p class="p-atracao-modal2">Lorem ipsum dolor sit amet consectetur. 
+                            Fames metus ac egestas turpis ipsum Lorem 
+                            ipsum dolor sit amet consectetur. Lorem ipsum 
+                            dolor sit amet consectetur. Fames metus ac egestas 
+                            turpis ipsum Lorem ipsum dolor sit amet consectetur.
+                            Lorem ipsum 
+                            dolor sit amet consectetur. Fames metus ac egestas 
+                            turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                    <button id="b-sari-modal2" class="close-modal" data-modal="modal2-card-ProEv">
+                        <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
+                    </button>
+                </div>
+            </dialog>
 
-                    <!-- MODAL 2 -->
-
-                        <dialog class="dlog-modal2-card-ProEv" id="modal2-card-ProEv">
-                            <div class="div-decorativa-modal2">
-                                <div id="linha-dec1-modal2"></div>
-                            </div>
-                            <div class="div-decorativa2-modal2">
-                                <div id="linha-dec2-modal2"></div>
-                                <div id="linha-dec3-modal2"></div>
-                                <div id="linha-dec4-modal2"></div>  
-                            </div>
-                            <div class="div-infs-evento-modal2">
-                                <div class="areatl-img-modal2-ProEv">
-                                    <div class="area-img-modal2-ProEv">
-                                        <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="area-text-modal2-ProEv">
-                                    <h1 class="title-atracao-modal2">Sampari</h1>
-                                    <p class="p-atracao-modal2">Lorem ipsum dolor sit amet consectetur. 
-                                        Fames metus ac egestas turpis ipsum Lorem 
-                                        ipsum dolor sit amet consectetur. Lorem ipsum 
-                                        dolor sit amet consectetur. Fames metus ac egestas 
-                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.
-                                        Lorem ipsum 
-                                        dolor sit amet consectetur. Fames metus ac egestas 
-                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
-                                </div>
-                                <button id="b-sari-modal2" class="close-modal" data-modal="modal2-card-ProEv">
-                                    <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
-                                </button>
-                            </div>
-                        </dialog>
         <!-- Imagens Decorativas -->
         <div class="bola-1">
             <img src="../../../Public/imgs/imagens-utilidades/Bola Nova 1 1 1.png" alt="">
         </div>
-
         <div class="bola-2">
             <img src="../../../Public/imgs/imagens-utilidades/Bola Nova 2 1 1.png" alt="">
         </div>
-
         <div class="bola-3">
             <img src="../../../Public/imgs/imagens-utilidades/Bola Nova 3 1.svg" alt="">
         </div>
-
     </main>
 
     <script src="../../../Public/js/js-modais/js-abrir-modal.js"></script>
