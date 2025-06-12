25d0abad31edd59e5da1ddfc646a7db7a350a8e1
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-06-12 16:48:20 -0400

    buttons

--- Public/js/js-adm/js-btns-padrao.js
+++ Public/js/js-adm/js-btns-padrao.js
@@ -13,12 +13,92 @@ btnVoltar.style.height = '40px'
 btnVoltar.style.borderRadius = '50%'
 btnVoltar.style.position = 'relative'
 btnVoltar.style.overflow = 'hidden'
+btnVoltar.style.cursor = 'pointer'
 
 let linhaCima = document.getElementById('linha1')
-linhaCima.style.width = '1.1rem'
+linhaCima.style.width = '.9rem'
 linhaCima.style.height = '.1rem'
 linhaCima.style.borderRadius = '.2rem'
 linhaCima.style.margin = 'auto'
-// linhaCima.style.transform. = ''
+linhaCima.style.transform = 'rotate(-40deg)'
+linhaCima.style.position = 'relative'
+linhaCima.style.top = '-.15rem'
+linhaCima.style.left = '-.1rem'
 linhaCima.style.backgroundColor = 'black'
 
+let linhaBaixo = document.getElementById('linha2')
+linhaBaixo.style.width = '.9rem'
+linhaBaixo.style.height = '.1rem'
+linhaBaixo.style.borderRadius = '.2rem'
+linhaBaixo.style.margin = 'auto'
+linhaBaixo.style.transform = 'rotate(40deg)'
+linhaBaixo.style.position = 'relative'
+linhaBaixo.style.top = '.25rem'
+linhaBaixo.style.left = '-.1rem'
+linhaBaixo.style.backgroundColor = 'black'
+
+let linhaCima2 = document.getElementById('linha3')
+linhaCima2.style.display = 'none'
+linhaCima2.style.width = '.9rem'
+linhaCima2.style.height = '.1rem'
+linhaCima2.style.borderRadius = '.2rem'
+linhaCima2.style.margin = 'auto'
+linhaCima2.style.transform = 'rotate(-40deg)'
+linhaCima2.style.position = 'relative'
+linhaCima2.style.top = '-.35rem'
+linhaCima2.style.left = '.3rem'
+linhaCima2.style.backgroundColor = 'black'
+
+let linhaBaixo2 = document.getElementById('linha4')
+linhaBaixo2.style.display = 'none'
+linhaBaixo2.style.width = '.9rem'
+linhaBaixo2.style.height = '.1rem'
+linhaBaixo2.style.borderRadius = '.2rem'
+linhaBaixo2.style.margin = 'auto'
+linhaBaixo2.style.transform = 'rotate(40deg)'
+linhaBaixo2.style.position = 'relative'
+linhaBaixo2.style.top = '.05rem'
+linhaBaixo2.style.left = '.3rem'
+linhaBaixo2.style.backgroundColor = 'black'
+
+btnVoltar.addEventListener('mouseover', () => {
+    linhaCima2.style.display = 'block'
+    linhaBaixo2.style.display = 'block'
+    linhaCima.style.left = `-.3rem`
+    linhaBaixo.style.left = `-.3rem`
+    
+})
+btnVoltar.addEventListener('mouseout', () => {
+    linhaCima2.style.display = 'none'
+    linhaBaixo2.style.display = 'none'
+    linhaCima.style.left = '-.1rem'
+    linhaBaixo.style.left = '-.1rem'
+})
+
+let btnsSalvarCancelar = document.getElementById('btns-salvar-cancelar')
+btnsSalvarCancelar.style.display = 'flex'
+btnsSalvarCancelar.style.alignItems = 'center'
+btnsSalvarCancelar.style.justifyContent = 'center'
+btnsSalvarCancelar.style.gap = '1rem'
+
+let btnReset = document.getElementById('btn-reset')
+btnReset.style.padding = '.5rem 1rem'
+btnReset.style.fontSize = '1.2rem'
+btnReset.style.fontWeight = '600'
+btnReset.style.border = 'none'
+btnReset.style.borderRadius = '.5rem'
+btnReset.style.backgroundColor = '#FF3877'
+btnReset.style.color = 'white'
+
+btnReset.addEventListener('mouseover', () => {
+    
+})
+
+let btnSalvar = document.getElementById('btn-salvar')
+btnSalvar.style.padding = '.5rem 1rem'
+btnSalvar.style.fontSize = '1.2rem'
+btnSalvar.style.fontWeight = '600'
+btnSalvar.style.border = 'none'
+btnSalvar.style.borderRadius = '.5rem'
+btnSalvar.style.backgroundColor = '#007E70'
+btnSalvar.style.color = 'white'
