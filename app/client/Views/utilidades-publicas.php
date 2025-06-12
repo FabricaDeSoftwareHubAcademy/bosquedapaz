067a2464ae71125ebfad9cbc67b5174ef5823ace
Gabriel-A77 <galvesdesouza386@gmail.com>
2025-06-12 15:55:35 -0400

    comit1

--- app/client/Views/utilidades-publicas.php
+++ app/client/Views/utilidades-publicas.php
@@ -86,8 +86,75 @@
             </div>
         </div>
 
+        <!-- MODAL 1 -->
+
+                        <dialog class="dlog-modal-card-ProEv" id="modal-card-ProEv">
+                            <div class="div-decorativa-modal">
+                                <div id="linha-dec1-modal"></div>
+                            </div>
+                            <div class="div-decorativa2-modal">
+                                <div id="linha-dec2-modal"></div>
+                                <div id="linha-dec3-modal"></div>
+                                <div id="linha-dec4-modal"></div>
+                            </div>
+                            <div class="div-infs-evento-modal">
+                                <div class="areatl-img-modal-ProEv">
+                                    <div class="area-img-modal-ProEv">
+                                        <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="area-text-modal-ProEv">
+                                    <h1 class="title-atracao-modal">Tapioca Forró</h1>
+                                    <p class="p-atracao-modal">Lorem ipsum dolor sit amet consectetur. 
+                                        Fames metus ac egestas turpis ipsum Lorem 
+                                        ipsum dolor sit amet consectetur. Lorem ipsum 
+                                        dolor sit amet consectetur. Fames metus ac egestas 
+                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.
+                                        Lorem ipsum 
+                                        dolor sit amet consectetur. Fames metus ac egestas 
+                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
+                                </div>
+                                <button id="b-sari-modal" class="close-modal" data-modal="modal-card-ProEv">
+                                    <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
+                                </button>
+                            </div>
+                        </dialog>
+
+                    <!-- MODAL 2 -->
+
+                        <dialog class="dlog-modal2-card-ProEv" id="modal2-card-ProEv">
+                            <div class="div-decorativa-modal2">
+                                <div id="linha-dec1-modal2"></div>
+                            </div>
+                            <div class="div-decorativa2-modal2">
+                                <div id="linha-dec2-modal2"></div>
+                                <div id="linha-dec3-modal2"></div>
+                                <div id="linha-dec4-modal2"></div>  
+                            </div>
+                            <div class="div-infs-evento-modal2">
+                                <div class="areatl-img-modal2-ProEv">
+                                    <div class="area-img-modal2-ProEv">
+                                        <img src="../../../Public/imgs/Proximos-Eventos-img/img-atracao-proev.png" alt="">
+                                    </div>
+                                </div>
+                                <div class="area-text-modal2-ProEv">
+                                    <h1 class="title-atracao-modal2">Sampari</h1>
+                                    <p class="p-atracao-modal2">Lorem ipsum dolor sit amet consectetur. 
+                                        Fames metus ac egestas turpis ipsum Lorem 
+                                        ipsum dolor sit amet consectetur. Lorem ipsum 
+                                        dolor sit amet consectetur. Fames metus ac egestas 
+                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.
+                                        Lorem ipsum 
+                                        dolor sit amet consectetur. Fames metus ac egestas 
+                                        turpis ipsum Lorem ipsum dolor sit amet consectetur.</p>
+                                </div>
+                                <button id="b-sari-modal2" class="close-modal" data-modal="modal2-card-ProEv">
+                                    <img src="../../../Public/imgs/Proximos-Eventos-img/arrow-circle-left.png" alt="">
+                                </button>
+                            </div>
+                        </dialog>
         <!-- Imagens Decorativas -->
-        <!-- <div class="bola-1">
+        <div class="bola-1">
             <img src="../../../Public/imgs/imagens-utilidades/Bola Nova 1 1 1.png" alt="">
         </div>
 
@@ -97,7 +164,7 @@
 
         <div class="bola-3">
             <img src="../../../Public/imgs/imagens-utilidades/Bola Nova 3 1.svg" alt="">
-        </div> -->
+        </div>
 
     </main>
 
