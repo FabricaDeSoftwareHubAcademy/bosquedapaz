16968bc192dcc3cf7cdcdea0a9fc35841c1bcc0f
Gabriel-A77 <galvesdesouza386@gmail.com>
2025-06-18 15:12:32 -0400

    comit

---
+++ app/Views/Client/utilidades-publicas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-utilidades-publicas.css">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-menu.css">
+    <title>Document</title>
+</head>
+<body>
+<?php include "../../../Public/include/home/menu-home.html" ?>
+
+<main>
+    <section class="tela-ult">
+
+    </section>
+</main>
+
+</body>
+</html>
