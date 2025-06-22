090890b36a0f8208a7a6f91eb588a958bccd7aae
halysonreis <halysondasilvadosreis@gmail.com>
2025-06-22 17:54:49 -0400

    modais completos

---
+++ Public/js/js-modais/js-modal-carregar.js
@@ -0,0 +1,24 @@
+const headElement  = document.getElementsByTagName('head')[0];
+const link  = document.createElement('link');
+link.rel  = 'stylesheet';
+link.type = 'text/css';
+link.href = 'http://localhost/bosquedapaz/Public/css/css-modais/style-modal-carregar.css';
+headElement.appendChild(link);
+
+
+const linkBootstrap = document.createElement('link')
+linkBootstrap.rel = 'stylesheet'
+linkBootstrap.href = 'https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css'
+headElement.appendChild(linkBootstrap)
+
+function openModalLoading() {
+    let modal = document.getElementById('modal-loading')
+    modal.showModal()
+}
+
+function closeModalLoading() {
+    let modal = document.getElementById('modal-loading')
+    modal.close()
+}
+
+document.addEventListener('DOMContentLoaded', openModalLoading)
