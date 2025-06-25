c2ff30edda7779a5613e0fcef678b2cdfd4a955c
Raquel Leirião Marques <raquelmarques2503@gmail.com>
2025-06-25 15:24:18 -0400

    .

---
+++ app/Views/Adm/cadastrar-boleto.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link 
+    rel="stylesheet" 
+    href="https://fonts.googleapis.com/css?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
+    <title>Cadastro de Boleto</title>
+    <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
+    <link rel="stylesheet" href="../../../Public/css/css-adm/style-cadastro-boleto.css">
+    <script src="../../../Public/js/js-adm/input-cadastro-boleto.js" defer></script>
+    
+</head>
+<body>
+<?php include "../../../Public/include/menu-adm.html" ?>
+
+    <main class="principal">
+        <div class="box">
+            <div class="container">
+                <div class="title">
+                    <h1>CADASTRO DE BOLETO</h1>
+                </div>
+
+                <div class="search">
+                    <input type="text" id="searchInput" placeholder="Pesquisar por expositor...">
+                    <label for="searchInput" class="label">
+                        <span class="material-symbols-outlined" id="lupa"> search </span>
+                    </label>
+                </div>
+            </div>
+
+            <form method="POST">
+
+                <div class="formularios">
+                    
+                    <div class="form1">
+
+                        <div class="input">
+                            <label class="label" for="expositor">Expositor:</label>
+                            <input type="text" name="nome-exp" id="nome-exp" class="form-input" placeholder="Nome do Expositor" required>
+                        </div>
+
+                        <div class="input">
+                            <label class="label">Arquivo em PDF:</label>
+                            <label for="aquivo" class="label">
+                                <span id="file-text" class="form-input">Selecionar Arquivo em PDF</span>
+                                <img class="img-input" src="../../../Public/imgs/edicoes-passadas-img/Upload.svg" alt="">
+                            </label>
+                            <input type="file" name="arquivo" id="arquivo" accept=".pdf" class="form-input" required>
+                        </div>
+
+                        <div class="input">
+                            <label class="label" for="valor">Valor:</label>
+                            <input type="text" name="valor" id="valor" class="form-input" placeholder="R$ 0,00" autocomplete="off" required>
+                        </div>
+
+                    </div>
+
+                    <div class="form2">
+                        
+                        <div class="input">
+                            <label for="cnpj-cpf" class="label">CNPJ/CPF:</label>
+                            <input type="text" name="cnpj-cpf" id="cnpj-cpf" class="form-input" placeholder="000.000.000-00  |  00.000.000/0000-00" required>
+                        </div>
+
+                        <div class="input">
+                            <label for="referencia" class="label">Referência:</label>
+                            <select name="input_select" id="referencia_select" class="form-input" required>
+                                <option id="option-cb" value="">Janeiro</option>
+                                <option id="option-cb" value="">Fevereiro</option>
+                                <option id="option-cb" value="">Março</option>
+                                <option id="option-cb" value="">Abril</option>
+                                <option id="option-cb" value="">Maio</option>
+                                <option id="option-cb" value="">Junho</option>
+                                <option id="option-cb" value="">Julho</option>
+                                <option id="option-cb" value="">Agosto</option>
+                                <option id="option-cb" value="">Setembro</option>
+                                <option id="option-cb" value="">Outubro</option>
+                                <option id="option-cb" value="">Novembro</option>
+                                <option id="option-cb" value="">Dezembro</option>
+                            </select>
+                        </div>
+
+                        <div class="input">
+                            <label for="vencimento" class="label"></label>
+                            <input type="date" name="val" id="val" class="form-input" placeholder="00/00/0000" required>
+                        </div>
+                    </div>
+
+                    <div class="btn">
+                        <div class="btn-fim">
+                            <button class="btn-cancelar">cancelar</button>
+                            <button name="REQUEST_METHOD" class="btn-salvar">salvar</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="button">
+                    <a href="Area-Adm.php" class="voltar">
+                        <img src="../../../Public/assets/faleconosco-voltar.png" alt="Botão de voltar" class="btn-voltar">
+                    </a>
+                </div>
+            </form>
+        </div>
+
+    </main>
+
+    <div class="formas">
+        <img src="../../../Public/imgs/imagens-bolas/bola-verde1.png" alt="Forma 1" class="forma1">
+        <img src="../../../Public/imgs/imagens-bolas/bola-verde2.png" alt="Forma 2" class="forma2">
+        <img src="../../../Public/imgs/imagens-bolas/bola-rosa.png" alt="Forma3" class="forma3">
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
+    <script src="../../../Public/js/js-menu/js-menu.js" defer></script>
+    <script>
+        $('#valor').mask('000.000.000.000.000,00', {reverse: true});
+    </script>
+</body>
+</html>
