9746efccb400b1d8b6503dc504e3511a667800c9
Teodororibas <teodororibas@icloud.com>
2025-07-03 17:18:03 -0400

    mudança de endereço das bolas e removi as lixeiras das listas

--- app/Views/Adm/listar-parceiros.php
+++ app/Views/Adm/listar-parceiros.php
@@ -1,6 +1,5 @@
 <?php require_once __DIR__ . '/../../../app/helpers/auth.php';?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,8 +12,6 @@
     <link rel="stylesheet" href="../../../Public/css/css-adm/style-listar-parceiros.css">
     <link rel="stylesheet" href="../../../Public/css/menu-adm.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
 </head>
 
 <body>
@@ -51,12 +48,8 @@
                                     <a href="#modal-editar" class="edit-icon">
                                         <i class="fa-solid fa-pen-to-square open-modal" data-modal="edit-modal"></i>
                                     </a>
-                                    <a href="#modal-recusar" class="delete-icon">
-                                        <i class="fa-solid fa-trash open-modal" data-modal="delete-modal"></i>
-                                    </a>
                                 </td>
-                            </tr>
-                            
+                            </tr>                         
                             <tr>
                                 <td class="usuario-col">Prefeitura de Campo Grande</td>
                                 <td>Rosângela</td>
@@ -66,9 +59,6 @@
                                 <td>
                                     <a href="#modal-editar" class="edit-icon">
                                         <i class="fa-solid fa-pen-to-square open-modal" data-modal="edit-modal"></i>
-                                    </a>
-                                    <a href="#modal-recusar" class="delete-icon">
-                                        <i class="fa-solid fa-trash open-modal" data-modal="delete-modal"></i>
                                     </a>
                                 </td>
                             </tr>
@@ -82,9 +72,6 @@
                                     <a href="#modal-editar" class="edit-icon">
                                         <i class="fa-solid fa-pen-to-square open-modal" data-modal="edit-modal"></i>
                                     </a>
-                                    <a href="#modal-recusar" class="delete-icon">
-                                        <i class="fa-solid fa-trash open-modal" data-modal="delete-modal"></i>
-                                    </a>
                                 </td>
                             </tr>
                             <tr>
@@ -96,9 +83,6 @@
                                 <td>
                                     <a href="#modal-editar" class="edit-icon">
                                         <i class="fa-solid fa-pen-to-square open-modal" data-modal="edit-modal"></i>
-                                    </a>
-                                    <a href="#modal-recusar" class="delete-icon">
-                                        <i class="fa-solid fa-trash open-modal" data-modal="delete-modal"></i>
                                     </a>
                                 </td>
                             </tr>
@@ -112,104 +96,24 @@
                                     <a href="#modal-editar" class="edit-icon">
                                         <i class="fa-solid fa-pen-to-square open-modal" data-modal="edit-modal"></i>
                                     </a>
-                                    <a href="#modal-recusar" class="delete-icon">
-                                        <i class="fa-solid fa-trash open-modal" data-modal="delete-modal"></i>
-                                    </a>
                                 </td>
                             </tr>
-
-
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="btns">
+        <!-- <div class="btns">
             <a href="Area-Adm.php" class="voltar">
                 <img src="../../../Public/imgs/img-listar-colaboradores/btn-voltar.png" alt="Botão de voltar" class="btn-voltar">
             </a>
-        </div>
-
-        <!-- Modal de Editar -->
-        <div class="acao-editar" id="modal-editar">
-            <div class="acao-content-editar">
-                <h1 class="acao-texto-editar">Editar parceiro</h1>
-                <!-- Contêiner para os inputs com a classe 'acao-inputs' -->
-                <div class="acao-inputs">
-                    <div class="input-group">
-                        <label for="input1" class="acao-label">Parceiro</label>
-                        <input id="input1" class="acao-input-edit" type="text" value="SENAC-Serviço Nacional de Aprendizagem Comercial">
-                    </div>
-                    <div class="input-group">
-                        <label for="input2" class="acao-label">Telefone</label>
-                        <input id="input2" class="acao-input-edit" type="text" value="(67) 3312-6260">
-                    </div>
-                    <div class="input-group">
-                        <label for="input3" class="acao-label">Email</label>
-                        <input id="input3" class="acao-input-edit" type="text" value="atendimento@ms.senac.br">
-                    </div>
-                    <div class="input-group">
-                        <label for="input4" class="acao-label">Contato</label>
-                        <input id="input4" class="acao-input-edit" type="text" value="João Pedro Costa Silva">
-                    </div>
-                    <div class="input-group">
-                        <label for="input5" class="acao-label">Tipo</label>
-
-                        <select name="todas_categorias" id="todas_categorias" class="acao-input-edit">
-
-                            <option value="">selecione</option>
-                            <option value="artesanato">Fisica</option>
-                            <option value="gastronia">Jurídica</option>
-
-                        </select>
-                    </div>
-                    <div class="input-group">
-                        <label for="input6" class="acao-label">CPF/CNPJ</label>
-                        <input id="input6" class="acao-input-edit" type="text" value="03.743.319/0001-52">
-                    </div>
-                    <div class="input-group">
-                        <label for="input7" class="acao-label">Logo</label>
-                        <input type="file" class="acao-input-edit" name="file" id="file" required>
-                    </div>
-                    <div class="input-group">
-                        <label for="input8" class="acao-label">CEP</label>
-                        <input id="input8" class="acao-input-edit" type="text" value="79002-141">
-                    </div>
-                </div>
-                <div class="acao-botoes-editar">
-                    <a href=""><button class="botao-cancelar">Cancelar</button></a>
-                    <a href="#recusado-sucesso"><button class="botao-confirmar">Salvar</button></a>
-                </div>
-            </div>
-        </div>
-
-
-        <!-- Modal de Delete -->
-        <div class="acao-recusar" id="modal-recusar">
-            <div class="acao-content-recusar">
-                <h1 class="acao-texto-recusar">Deseja excluir o parceiro?</h1>
-                <div class="acao-botoes-recusar">
-                    <a href=""><button class="botao-cancelar">Cancelar</button></a>
-                    <a href="#recusado-sucesso"><button class="botao-confirmar">Excluir</button></a>
-                </div>
-            </div>
-        </div>
-
-        <!-- Modal confirmação -->
-        <div class="mensagem-recusar" id="recusado-sucesso">
-            <div class="mensagem-content-recusar">
-                <h1 class="mensagem-texto-recusar">Concluído com sucesso!</h1>
-                <a href="#"><button class="botao-confirmar">Confirmar</button></a>
-            </div>
-        </div>
-
-
+        </div> -->
     </main>
 
     <div class="bolas-fundo">
-        <img src="../../../Public/imgs/imagens-bolas/bola azul1.png" alt="Bola Fundo 1" class="bola-verde1">
-        <img src="../../../Public/imgs/imagens-bolas/bola azul2.png" alt="Bola Fundo 2" class="bola-verde2">
-        <img src="../../../Public/imgs/imagens-bolas/bola azu.png" alt="Bola Fundo 3" class="bola-rosa">
+        <img src="../../../Public/assets/img-bolas/bola-verde1.png" alt="Bola de Fundo 1" class="bola-verde1">
+        <img src="../../../Public/assets/img-bolas/bola-verde2.png" alt="Bola de Fundo 2" class="bola-verde2">
+        <img src="../../../Public/assets/img-bolas/bola-rosa.png" alt="Bola de Fundo 3" class="bola-rosa">
     </div>
 
     <script src="../../../Public/js//js-adm/status-colaborador.js"></script>
