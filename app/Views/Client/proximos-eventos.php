89c16ac0c038c29f3efb90ab434ff513207c13a2
Matheus Manja <matheusmanja7@gmail.com>
2025-07-03 00:22:10 -0400

    Comit Edições e Galeria
    Atualização de Edições passadas e Criação da tela Galeria de Imagens

--- app/Views/Client/proximos-eventos.php
+++ app/Views/Client/proximos-eventos.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../Public/css/css-home/style-proximos-eventos.css">
-    <title>Bosque da Paz</title>
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico" >
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-proximos-eventos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <title>Bosque da Paz</title>
 </head>
 <body>
+    <!-- Include do Menu:  -->
     <?php include "../../../Public/include/home/menu-home.html"?>
     
     <main>
