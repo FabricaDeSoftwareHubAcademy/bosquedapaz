be385ec5038e1675acf3fb80a240830f6258ef30
halysonreis <halysondasilvadosreis@gmail.com>
2025-07-06 23:35:27 -0400

    novo front cadastro expositor

--- app/Views/Client/cadastro-expositor-client.php
+++ app/Views/Client/cadastro-expositor-client.php
@@ -1,6 +1,3 @@
-<?php require_once __DIR__ . '/../../../app/helpers/auth.php'; ?>
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -8,128 +5,106 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adm - Bosque da Paz</title>
-    <link rel="stylesheet" href="../../../Public/css/css-adm/style-cadastrar-expositor.css">
     <link rel="shortcut icon" href="../../../Public/assets/icons/folha.ico">
+    <link rel="stylesheet" href="../../../Public/css/css-home/style-cadastrar-expositor.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 
-<body onload="getCategorias()">
+<body>
     <?php include "../../../Public/include/home/menu-home.html" ?>
 
     <main class="principal">
+        <form class="form-cadastro" id="formCadastro" enctype="multipart/form-data">
+            <h1 class="title-cadastro">Cadastro expositor</h1>
+            <div class="conteiner_inputs">
 
-        <div class="box">
-            <div class="title">
-                <h1 class="title-text">CADASTRO DE EXPOSITORES</h1>
-            </div>
-
-            <form id="fomulario_cad_expositor" method="POST">
-                <div class="formularios">
-                    <div class="form-pessoa">
-                        <div class="input">
-                            <label>Nome completo:</label>
-                            <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo" required>
-                        </div>
-                        <div class="input">
-                            <label>Whatsapp:</label>
-                            <input type="tel" name="whats" id="whats" placeholder="Número de whatsapp" oninput="formatWhatsAppNumber(this)">
-                        </div>
-                        <div class="input">
-                            <label>E-mail:</label>
-                            <input type="text" name="email" id="email" placeholder="Digite seu e-mail" required>
-                        </div>
-                        <div class="input">
-                            <label>Qual Cidade Reside:</label>
-                            <input type="text" name="cidade" id="cidade" placeholder="Digite sua cidade">
-                        </div>
+                <div class="conteiner-input">
+                    <div class="content-input">
+                        <label class="label-input">Nome completo:</label>
+                        <input class="input" type="text" name="" id="" placeholder="Seu nome" required>
                     </div>
-
-                    <div class="form-loja">
-                        <div class="input">
-                            <label>Produto:</label>
-                            <input type="text" name="produto" id="produto" placeholder="Digite seu produto" required>
-                        </div>
-
-                        <div class="input">
-                            <label>Marca:</label>
-                            <input type="text" name="marca" id="marca" placeholder="Digite a marca " required>
-                        </div>
-
-                        <div class="input">
-                            <label for="optionInput3">Categorias</label>
-                            <select name="id_categoria" id="categorias" class="" require>
-                                <!-- OPTIONS GERADOS PELO JS -->
-                            </select>
-                        </div>
-
-                        <div class="input">
-                            <label>Link instagram:</label>
-
-                            <input type="text" name="link_instagram" id="link_instagram" placeholder="link instagram" required>
-                        </div>
+                    <div class="content-input">
+                        <label class="label-input">Nome da marca:</label>
+                        <input class="input" type="text" name="" id="" placeholder="Nome da sua marca" required>
                     </div>
-
-                    <div class="form-expo">
-                        <div id="tipo_expo" class="form-group">
-                            <label id="expo_label" for="tipo-expo">Tipo de exposição:</label>
-                            <select name="tipo" id="tipo_expo" class="select">
-                                <option value="">Selecione</option>
-                                <option value="trailer">Trailer</option>
-                                <option value="food-truck">Food truck</option>
-                                <option value="barraca">Barraca</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group-energia">
-                            <label id="energia_label" for="energia">Precisa de energia?</label>
-                            <select name="energia" id="energia" class="select">
-                                <option value="">Selecione</option>
-                                <option value="sim">Sim</option>
-                                <option value="nao">Não</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label id="volt_label" for="equipamentos">Voltagens dos equipamentos</label>
-                            <select name="voltagem" id="voltagem" class="select">
-                                <option value="">selecione</option>
-                                <option value="110v">110v</option>
-                                <option value="220v">220v</option>
-                            </select>
-                        </div>
-
-                        <div class="form-files">
-                            <label>Escolher foto de perfil:</label>
-                            <input type="file" name="img_perfil" id="files" multiple="multiple">
-                        </div>
-                    </div>
-
                 </div>
 
-                <?php include '../../../Public/include/Butons-forms.html'; ?>
+                <div class="conteiner-input">
+                    <div class="content-input">
+                        <label class="label-input">Email:</label>
+                        <input class="input" type="email" name="" id="" placeholder="Exemplo@gmail.com" required>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Logo da Marca:</label>
+                        <input class="input" type="file" name="" id="" required>
+                    </div>
+                </div>
 
-            </form>
+                <div class="conteiner-input">
+                    <div class="content-input">
+                        <label class="label-input">Qual cidade reside:</label>
+                        <input class="input" type="text" name="" id="" placeholder="EX: Campo Grande" required>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Número de whatsapp:</label>
+                        <input class="input" type="tel" name="" id="" placeholder="EX: 67 9 xxxx-xxxx" required>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Rede social:</label>
+                        <input class="input" type="text" name="" id="" placeholder="link ou @usuário" required>
+                    </div>
+                </div>
 
-            <div class="overlay" id="overlay"></div>
-            <?php include "../../../Public/include/modais/modal-Confirmar.html"; ?>
-            <?php include "../../../Public/include/modais/modal-sucesso.html"; ?>
-            <?php include "../../../Public/include/modais/modal-error.html"; ?>
+                <div class="conteiner-input">
+                    <div class="content-input">
+                        <label class="label-input">Qual seu produto:</label>
+                        <input class="input" type="text" name="" id="" placeholder="EX: Açai" required>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Qual sua categoria:</label>
+                        <select class="input" name="" id="" required>
+                            <option value="">a</option>
+                            <option value="">b</option>
+                            <option value="">c</option>
+                            <option value=""></option>
+                        </select>
+                    </div>
+                </div>
 
+                <div class="conteiner-input">
+                    <div class="content-input">
+                        <label class="label-input">Se precisa de energia, descreva quais voltagens e equipamentos: Importante</label>
+                        <textarea class="textarea" name="" id="" placeholder="EX: 127v/220v, freezer, geladeira"></textarea>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Se possui um trailer, foodtruck ou barraca de comida, quais as medidas exatas dele? Importante</label>
+                        <textarea class="textarea" name="" id="" placeholder="EX: 2Ms X 3Ms"></textarea>
+                    </div>
+                </div>
+
+                <div class="conteiner-input">
+                    <div class="content-input maior">
+                        <label class="label-input">Descreva a sua marca:</label>
+                        <textarea class="textarea" name="" id="" required></textarea>
+                    </div>
+                    <div class="content-input">
+                        <label class="label-input">Envie 6 fotos do seu produto para avaliação: importante</label>
+                        <input class="input" type="file" name="" id="" required>
+                    </div>
+                </div>
+            </div>
+            <?php include "../../../Public/include/Butons-forms.html" ?>
+        </form>
+        <div class="bolas">
+            <img src="../../../Public/assets/img-bolas/bola-verde1.png" class="bola-verde1">
+            <img src="../../../Public/assets/img-bolas/bola-verde2.png" class="bola-verde2">
+            <img src="../../../Public/assets/img-bolas/bola-rosa.png" class="bola-rosa">
         </div>
     </main>
 
-    <div class="bolas-fundo">
-
-        <img src="../../../Public/assets/img-bolas/bola-verde1.png" alt="Bola Fundo 1" class="bola-verde1">
-        <img src="../../../Public/assets/img-bolas/bola-verde2.png" alt="Bola Fundo 2" class="bola-verde2">
-        <img src="../../../Public/assets/img-bolas/bola-rosa.png" alt="Bola Fundo 3" class="bola-rosa">
-
-    </div>
-
-
-
 
     <script src="../../../Public/js/js-menu/js-menu.js" defer></script>
-    <script src="../../../Public/js/js-adm/js-cadastrar-expositor.js" defer></script>
+    <!-- <script src="../../../Public/js/js-adm/js-cadastrar-expositor.js" defer></script> -->
 
 </body>
 
