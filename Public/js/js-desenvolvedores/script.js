78632c5a5a423858da60da7ae8bd86ab77396619
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-07-15 15:54:08 -0400

    pag devs

--- Public/js/js-desenvolvedores/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.btn-default-mobile').on('click',function(){
-        $('.mobile-menu').toggleClass('active');
-        $('.btn-default-mobile').find('i').toggleClass('fa-x');
-    });
-})
