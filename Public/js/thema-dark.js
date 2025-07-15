da19038d425c182f52b04617fe5e20bcf5a057c9
Gabriel-A77 <galvesdesouza386@gmail.com>
2025-07-15 15:31:07 -0400

    atuali

--- Public/js/thema-dark.js
+++ Public/js/thema-dark.js
@@ -1,23 +1,40 @@
 let cabecalho = document.querySelector('header')
 let body = document.querySelector('body')
 let nav = document.querySelectorAll('a')
+let conteiner = document.querySelectorAll('section')
 let theme = 0
+
+if(localStorage.getItem('theme') === 'dark') {
+    aplicarTemaEscuro()
+    theme = 1
+}
 
 let btn = document.getElementById('theme-toggle')
 btn.addEventListener('click', () => {
-    if(theme == 0){
-        cabecalho.style.setProperty('background-color', 'black')
-        body.style.setProperty('background-color', 'black')
+    if(theme === 0){
+        aplicarTemaEscuro()
         theme = 1
-
-        nav.forEach(e => {
-            e.style.setProperty('color', 'white', 'important')
-        });
+        localStorage.setItem('theme', 'dark')
     }
-    else if(theme = 1){
-        cabecalho.style.setProperty('background-color', 'white')
-        body.style.setProperty('background-color', 'white')
+    else {
+        aplicarTemaClaro()
         theme = 0
+        localStorage.setItem('theme', 'light')
     }
 })
 
+function aplicarTemaEscuro() {
+    cabecalho.style.setProperty('background-color', 'black')
+    body.style.setProperty('background-color', 'black')
+    nav.forEach(e => {
+        e.style.setProperty('color', 'white', 'important')
+    })
+}
+
+function aplicarTemaClaro() {
+    cabecalho.style.setProperty('background-color', 'white')
+    body.style.setProperty('background-color', 'white')
+    nav.forEach(e => {
+        e.style.setProperty('color', 'black', 'important')
+    })
+}
