2bd8b5ea73c5aaf3477d1f68c0c313e5274820f4
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-07-16 16:21:52 -0400

    alteração

--- Public/js/js-recuperar-senha/recuperar-senha-nova-senha.js
+++ Public/js/js-recuperar-senha/recuperar-senha-nova-senha.js
@@ -3,25 +3,21 @@ linkSuscess.href = '../Public/css/css-modais/style-modal-sucesso.css';
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault(); // Impede envio imediato
-
     const senha = document.getElementById("novaSenha").value.trim();
     const confSenha = document.getElementById("confirmSenha").value.trim();
 
     if (senha === "" || confSenha === "") {
         alert("Preencha todos os campos");
+        e.preventDefault();
         return;
     }
 
     if (senha !== confSenha) {
         alert("Senha e confirmação não conferem");
+        e.preventDefault();
         return;
     }
 
+    // Modal opcional
     openModalSucesso();
-    // window.location.href = "./tela-login.php"; 
-
-    setTimeout(() => {
-        window.location.href = "./tela-login.php"; 
-    }, 1500);
 });
