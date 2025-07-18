425184e554e31f30830986dbc6cb7d8e26321144
ThiegoFerreira <thiegocpf@gmail.com>
2025-07-18 14:01:49 -0400

    pronto

--- actions/Login.php
+++ actions/Login.php
@@ -16,18 +16,18 @@ class Login{
         $pessoa = $db->select("email = '$email'")->fetchObject();
 
         // // Validação simples, sem hash
-        // if ($pessoa && $pessoa->senha === $senha) {
-        //     return $pessoa;
-        // } else {
-        //     return null;
-        // }
-
-        // Validação com hash
-        if($pessoa && password_verify($senha, $pessoa->senha)){
+        if ($pessoa && $pessoa->senha === $senha) {
             return $pessoa;
         } else {
             return null;
         }
+
+        // Validação com hash
+        // if($pessoa && password_verify($senha, $pessoa->senha)){
+        //     return $pessoa;
+        // } else {
+        //     return null;
+        // }
     }
 
 }
