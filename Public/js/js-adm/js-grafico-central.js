47ef39d5ea7f66ec5a39c80c8822a8264504eced
EnzoArimura15 <enzo.arimura@gmail.com>
2025-07-22 17:34:35 -0400

    Update grafico

---
+++ Public/js/js-adm/js-grafico-central.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', async () => {
+
+    try {
+        const response = await fetch('../../../actions/action-grafico-central.php');
+
+        if (!response.ok) throw new Error('Erro na requisição');
+
+        const data = await response.json();
+
+        let qtdExpositor = data.qtdExpositor;
+
+        let card = document.getElementById("dadosQtdExpositores");
+        let html = '';
+
+        html += `
+            <p id="qtdExpositores" class="numeros">${qtdExpositor}</p>
+            <p class="feirantes">Quantidade de Expositores</p>
+        `;
+
+        card.innerHTML = html;
+
+
+        // if (data.status === 'success') {
+        //     renderizarTabela(data.dados);
+        //     aplicarEventosBotoes(); // Aplica eventos depois de gerar a tabela
+        // } else {
+        //     tabelaCategoria.innerHTML = `<tr><td colspan="4">Erro ao carregar categorias</td></tr>`;
+        // }
+    } catch (error) {
+        console.error('Erro:', error);
+        // tabelaCategoria.innerHTML = `<tr><td colspan="4">Erro ao carregar categorias</td></tr>`;
+    }
+
+    
+})
---
+++ actions/action-grafico-central.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../vendor/autoload.php');
+use app\Controller\Expositor;
+
+header('Content-Type: application/json');
+
+// 🚦 Apenas requisições GET com um ID são permitidas
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $expositorController = new Expositor();
+    $expositor = $expositorController->listar();
+
+    if ($expositor) {
+        // Se a categoria foi encontrada, retorna os dados
+        echo json_encode([
+            'status' => 'success',
+            'qtdExpositor' => count($expositor),
+        ]);
+    } else {
+        // Se não encontrou, retorna um erro
+        echo json_encode(['status' => 'error', 'mensagem' => 'Categoria não encontrada']);
+    }
+} else {
+    // Se a requisição for inválida, retorna um erro
+    echo json_encode(['status' => 'error', 'mensagem' => 'Requisição inválida ou ID não fornecido.']);
+}
