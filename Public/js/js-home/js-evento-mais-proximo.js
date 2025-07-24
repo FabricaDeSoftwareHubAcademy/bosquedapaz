3afc00db13ce1e85dd76d97e469010c9730a4489
ThiegoFerreira <thiegocpf@gmail.com>
2025-07-24 11:15:43 -0400

    Tudo no jeito.
    Na home é exibido a imagem do evento com data mais próxima.

---
+++ Public/js/js-home/js-evento-mais-proximo.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const res = await fetch('../../../actions/action-evento-mais-proximo.php');
+        const json = await res.json();
+
+        if (json.status === 'success') {
+            const evento = json.evento;
+
+            
+            const imgPrincipal = document.querySelector('.img_prox_event');
+            const imgPequena = document.querySelector('.img_prox_event_pequena');
+
+            const caminhoBanner = `../../../Public/${evento.banner_evento}`;
+            if (imgPrincipal) imgPrincipal.src = caminhoBanner;
+            if (imgPequena) imgPequena.src = caminhoBanner;
+
+
+        } else {
+            console.warn('Evento mais próximo não encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar evento mais próximo:', error);
+    }
+
+    function formatarDataBR(data) {
+        const [ano, mes, dia] = data.split('-');
+        return `${dia}/${mes}/${ano}`;
+    }
+});
