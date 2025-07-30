80353432790023c9836bbff001fc6c76cf80fa58
gcamargogy <camargogdy@hotmail.com>
2025-07-30 16:36:28 -0400

    .

--- actions/action-cadastrar-boletos.php
+++ actions/action-cadastrar-boletos.php
@@ -1,59 +1,87 @@
 <?php
 require_once('../vendor/autoload.php');
 use app\Controller\Boleto;
+use setasign\Fpdi\Fpdi;
+use setasign\Fpdi\PdfParser\StreamReader;
 
 $caminhoPasta = '../Public/uploads/uploads-boletos';
 
 if (isset($_POST['botao-cadastrar'])) {
-    
     if (isset($_FILES['arquivo']) && $_FILES['arquivo']['error'] == 0) {
-        
+
         if (!is_dir($caminhoPasta)) {
             mkdir($caminhoPasta, 0755, true);
         }
 
         $nomePessoa = $_POST['nome_exp'] ?? '';
-
         $nomeLimpo = preg_replace('/[^a-z0-9]/', '_', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $nomePessoa)));
 
-        $mesReferencia = strtolower($_POST['referencia_input'] ?? '');
+        $valorOriginal = floatval(str_replace(',', '.', $_POST['valor_input']));
+        $vencimentoInicial = $_POST['vencimento_input'] ?? '';
+        $idExpositor = $_POST['id-expositor'];
 
-        $vencimento = $_POST['vencimento_input'] ?? '';
-
-        if ($vencimento) {
-            $data = DateTime::createFromFormat('Y-m-d', $vencimento);
-            if ($data) {
-                $dia = $data->format('d');
-                $mes = $data->format('m');
-                $ano = $data->format('Y');
-            } else {
-                $dia = $mes = $ano = 'data_invalida';
-            }
-        } else {
-            $dia = $mes = $ano = 'data_vazia';
+        if (!$vencimentoInicial) {
+            echo "<script>alert('Data de vencimento inválida!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
+            exit;
         }
 
-        $nomeArquivo = "boleto_{$nomeLimpo}_{$mesReferencia}_{$dia}_{$mes}_{$ano}.pdf";
+        $dataVencimento = DateTime::createFromFormat('Y-m-d', $vencimentoInicial);
+        if (!$dataVencimento) {
+            echo "<script>alert('Formato de data inválido!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
+            exit;
+        }
 
-        $caminhoCompleto = $caminhoPasta . '/' . $nomeArquivo;
+        try {
+            $pdf = new Fpdi();
+            $pageCount = $pdf->setSourceFile(StreamReader::createByFile($_FILES['arquivo']['tmp_name']));
+        } catch (Exception $e) {
+            echo "<script>alert('Erro ao ler o PDF: {$e->getMessage()}'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
+            exit;
+        }
 
-        if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $caminhoCompleto)) {
-            
+        $backupNome = "{$nomeLimpo}_" . date('Ymd_His') . "_completo.pdf";
+        move_uploaded_file($_FILES['arquivo']['tmp_name'], $caminhoPasta . '/' . $backupNome);
+
+        $sucesso = true;
+
+        for ($i = 0; $i < $pageCount; $i++) {
+            $dataAtual = clone $dataVencimento;
+            $dataAtual->modify("+{$i} month");
+
+            $fmt = new IntlDateFormatter(
+                'pt_BR',
+                IntlDateFormatter::FULL,
+                IntlDateFormatter::NONE,
+                'America/Sao_Paulo',
+                IntlDateFormatter::GREGORIAN,
+                'LLLL'
+            );
+            $mesNome = $fmt->format($dataAtual);
+            $mesNome = strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $mesNome));
+            $mesNome = preg_replace('/[^a-z]/', '', $mesNome);
+
+            $nomeArquivo = "{$nomeLimpo}_{$mesNome}.pdf";
+            $caminhoArquivo = $caminhoPasta . '/' . $nomeArquivo;
+
+            copy($caminhoPasta . '/' . $backupNome, $caminhoArquivo);
+
             $boleto = new Boleto();
-            $boleto->valor = floatval(str_replace(',', '.', $_POST['valor_input']));
+            $boleto->valor = $valorOriginal;
             $boleto->pdf = $nomeArquivo;
-            $boleto->mes_referencia = $_POST['referencia_input'];
-            $boleto->vencimento = $_POST['vencimento_input'];
-            $boleto->id_expositor = $_POST['id-expositor'];
+            $boleto->mes_referencia = ucfirst($mesNome);
+            $boleto->vencimento = $dataAtual->format('Y-m-d');
+            $boleto->id_expositor = $idExpositor;
 
-            if ($boleto->CadastrarBoletos()) {
-                echo "<script>alert('Boleto cadastrado com sucesso!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
-            } else {
-                echo "<script>alert('Erro ao cadastrar boleto!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
+            if (!$boleto->CadastrarBoletos()) {
+                $sucesso = false;
+                break;
             }
+        }
 
+        if ($sucesso) {
+            echo "<script>alert('Boletos cadastrados com sucesso!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
         } else {
-            echo "<script>alert('Erro ao mover o arquivo PDF!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
+            echo "<script>alert('Erro ao cadastrar um dos boletos!'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
         }
 
     } else {
@@ -64,4 +92,3 @@ if (isset($_POST['botao-cadastrar'])) {
     echo "<script>alert('Falha no Cadastro.'); window.location.href = '../app/Views/Adm/cadastrar-boleto.php';</script>";
     exit;
 }
-?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'desenvolvedor/bosquedapaz',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd2ec619b7436e15d5510c4a3d1eca92984f10cea',
+        'reference' => '17dd0cb72694b11c4f8f98b3d1ea677203996cff',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'desenvolvedor/bosquedapaz' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd2ec619b7436e15d5510c4a3d1eca92984f10cea',
+            'reference' => '17dd0cb72694b11c4f8f98b3d1ea677203996cff',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -25,6 +25,17 @@
             'reference' => '912f278a482f50b9e1b76ddfd2e00413cefb6747',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'setasign/fpdi' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4fc6d6e37029db275930e39e821d5f5aa3adcae1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../setasign/fpdi',
             'aliases' => array(
                 0 => '9999999-dev',
             ),
