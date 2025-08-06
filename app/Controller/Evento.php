fbe867d8c4b9eb90cd931bb1812c7d4bc1e74dd2
ThiegoFerreira <thiegocpf@gmail.com>
2025-08-06 14:43:18 -0400

    atualizando endereco

--- app/Controller/Evento.php
+++ app/Controller/Evento.php
@@ -11,29 +11,34 @@ class Evento
     public string $nome_evento;
     public string $subtitulo_evento;
     public string $descricao_evento;
-    public string  $data_evento;
+    public string $data_evento;
     public string $hora_inicio;
     public string $hora_fim;
-    public string $endereco_evento;
+    public string $id_endereco_evento;
     public int $status;
     public string $banner_evento;
 
 
     public function cadastrar_evento()
     {
-        $db = new Database('evento');
-        $res = $db->insert([
-            'nome_evento' => $this->nome_evento,
-            'subtitulo_evento' => $this->subtitulo_evento,
-            'descricao_evento' => $this->descricao_evento,
-            'data_evento' => $this->data_evento,
-            'hora_inicio' => $this->hora_inicio,
-            'hora_fim' => $this->hora_fim,
-            'id_endereco_evento' => $this->endereco_evento,
-            'banner_evento' => $this->banner_evento
-        ]);
+        try {
+            $db = new Database('evento');
+            $res = $db->insert([
+                'nome_evento' => $this->nome_evento,
+                'subtitulo_evento' => $this->subtitulo_evento,
+                'descricao_evento' => $this->descricao_evento,
+                'data_evento' => $this->data_evento,
+                'hora_inicio' => $this->hora_inicio,
+                'hora_fim' => $this->hora_fim,
+                'id_endereco_evento' => $this->id_endereco_evento,
+                'banner_evento' => $this->banner_evento
+            ]);
+    
+            return $res;
+        } catch (\Throwable $th) {
+            return FALSE;
+        }
 
-        return $res;
     }
 
     public function listar_evento($where = null,$order = null,$limit = null){
@@ -69,7 +74,7 @@ class Evento
         'data_evento' => $this->data_evento,
         'hora_inicio' => $this->hora_inicio,
         'hora_fim' => $this->hora_fim,
-        'id_endereco_evento' => $this->endereco_evento,
+        'id_endereco_evento' => $this->id_endereco_evento,
         'banner_evento' => $this->banner_evento,
         'status' => $this->status
     ];
