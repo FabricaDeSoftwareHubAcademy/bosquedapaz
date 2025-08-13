ad565f5b2d2d2dc53306313a21066a209041cd12
Matheus Manja <matheusmanja7@gmail.com>
2025-08-13 16:08:03 -0400

    comit atualiza

--- Public/include/home/carrossel-home.php
+++ Public/include/home/carrossel-home.php
@@ -1,21 +1,22 @@
-<!-- inicio carrosel -->
 <section class="carrossel" id="carrossel">
     <div class="conteiner-slider">
         <div class="slider-swap">
             <div class="swap" id="arrow-left">
-                <i class="bi bi-arrow-left arrow"></i>
+                <i class="bi bi-chevron-left arrow"></i>
             </div>
             <div class="swap" id="arrow-right">
-                <i class="bi bi-arrow-right arrow"></i>
+                <i class="bi bi-chevron-right arrow"></i>
             </div>
         </div>
+
         <div class="slider" id="slider">
         </div>
-    </div>
-    <div class="slider-balls">
-        <div class="ball" id="ball"></div>
-        <div class="ball" id="ball"></div>
-        <div class="ball" id="ball"></div>
+
+        <div class="slider-balls" id="slider-balls">
+            <div class="ball active"></div>
+            <div class="ball"></div>
+            <div class="ball"></div>
+        </div>
     </div>
 </section>
 
